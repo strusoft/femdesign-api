8ff402052a5a7b896d132e296af488e318f44105
Andreas Oscarsson <andreas.oscarsson@strusoft.com>
2020-06-16 13:34:27 +0200

    Added storeys

---
+++ src/GenericClasses/RestrictedObject.cs
@@ -0,0 +1,47 @@
+// https://strusoft.com/
+using System.Collections.Generic;
+
+namespace FemDesign
+{
+    /// <summary>
+    /// Contains string restrictions used for property definitions.
+    /// 
+    /// Restrictions are defined by strusoft.xsd
+    /// </summary>
+    public class RestrictedObject
+    {
+        /// <summary>
+        /// Check if val is a non zero 3d vector.
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        internal static Geometry.FdVector3d NonZeroFdVector3d(Geometry.FdVector3d val)
+        {
+            if (val.IsZero())
+            {
+                throw new System.ArgumentException("Vector must be non zero.");
+            }
+            else
+            {
+                return val;
+            }
+        }
+
+        /// <summary>
+        /// Check if val is a non zero 2d vector.
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        internal static Geometry.FdVector2d NonZeroFdVector2d(Geometry.FdVector2d val)
+        {
+            if (val.IsZero())
+            {
+                throw new System.ArgumentException("Vector must be non zero.");
+            }
+            else
+            {
+                return val;
+            }
+        }
+    }
+}
---
+++ src/StructureGrid/Storey.cs
@@ -0,0 +1,96 @@
+using System.Xml.Serialization;
+
+#region dynamo
+using Autodesk.DesignScript.Runtime;
+#endregion
+
+namespace FemDesign.StructureGrid
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public class Storey: EntityBase
+    {
+        [XmlElement("origo", Order=1)]
+        public Geometry.FdPoint3d origo { get; set; }
+        [XmlElement("direction", Order=2)]
+        public Geometry.FdVector2d _direction;
+        [XmlIgnore]
+        public Geometry.FdVector3d direction
+        {
+            get
+            {
+                return this._direction.To3d();
+            }
+            set
+            {
+                this._direction = RestrictedObject.NonZeroFdVector2d(value.Normalize().To2d());
+            }
+        }
+
+        [XmlAttribute("dimension_x")]
+        public double _dimensionX; // positive double
+        [XmlIgnore]
+        public double dimensionX
+        {
+            get { return this._dimensionX; }
+            set { this._dimensionX = RestrictedDouble.Positive(value); }
+        }
+        [XmlAttribute("dimension_y")]
+        public double _dimensionY; // positive double
+        [XmlIgnore]
+        public double dimensionY
+        {
+            get { return this._dimensionY; }
+            set { this._dimensionY = RestrictedDouble.Positive(value); }
+        }
+        [XmlAttribute("name")]
+        public string name { get; set; }
+        
+        /// <summary>
+        /// Parameterless constructor for serialization
+        /// </summary>
+        private Storey()
+        {
+
+        }
+
+        /// <summary>
+        /// Internal constructor.
+        /// </summary>
+        /// <param name="origo">Origo of storey.</param>
+        /// <param name="direction">Direction of storey x'-axis. Optional, default value isGCS x-axis.</param>
+        /// <param name="dimensionX">Dimension in x'-direction.</param>
+        /// <param name="dimensionY">Dimension in y'-direction.</param>
+        /// <param name="name">Name of storey.</param>
+        internal Storey(Geometry.FdPoint3d origo, Geometry.FdVector3d direction, double dimensionX, double dimensionY, string name)
+        {
+            this.EntityCreated();
+            this.origo = origo;
+            this.direction = direction;
+            this.dimensionX = dimensionX;
+            this.dimensionY = dimensionY;
+            this.name = name;
+        }
+
+        #region dynamo
+        /// <summary>
+        /// Create a storey.
+        /// </summary>
+        /// <param name="origo">Origo of storey. Storeys can only have unique Z-coordinates. If several storeys are placed in a model their origos should share XY-coordinates.</param>
+        /// <param name="direction">Direction of storey x'-axis in the XY-plane. If several storeys are placed in a model their direction should be identical. Optional, default value is GCS x-axis.</param>
+        /// <param name="dimensionX">Dimension in x'-direction.</param>
+        /// <param name="dimensionY">Dimension in y'-direction.</param>
+        /// <param name="name">Name of storey.</param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(true)]
+        public static Storey Define(string name, Autodesk.DesignScript.Geometry.Point origo, [DefaultArgument("Autodesk.DesignScript.Geometry.Vector.XAxis()")] Autodesk.DesignScript.Geometry.Vector direction, double dimensionX = 50, double dimensionY = 30)
+        {
+            // convert geometry
+            Geometry.FdPoint3d p = Geometry.FdPoint3d.FromDynamo(origo);
+            Geometry.FdVector3d v = Geometry.FdVector3d.FromDynamo(direction);
+
+            // return
+            return new Storey(p, v, dimensionX, dimensionY, name);
+        }
+        #endregion
+    }
+}
