8fdf458a6560563b096de515bd16113ff451dce1
andosca <andreas.oscarsson@strusoft.com>
2020-08-24 09:44:52 +0200

    Add VirtualBar

--- src/AdvancedFem/AdvancedFem.cs
+++ src/AdvancedFem/AdvancedFem.cs
@@ -18,6 +18,8 @@ namespace FemDesign
         // surface_connection
 
         // virtual_bar
+        [XmlElement("virtual_bar")]
+        public List<VirtualBar> virtualBar = new List<VirtualBar>();
 
         // virtual_shell
 
