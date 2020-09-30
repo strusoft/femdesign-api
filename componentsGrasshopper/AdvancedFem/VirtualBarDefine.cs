0c2d92a990379c70d5303daf20d6f0566f3fe6a1
andosca <andreas.oscarsson@strusoft.com>
2020-09-30 09:27:17 +0200

    Change namespace and property naming

--- componentsGrasshopper/AdvancedFem/VirtualBarDefine.cs
+++ componentsGrasshopper/AdvancedFem/VirtualBarDefine.cs
@@ -111,7 +111,7 @@ namespace FemDesign.GH
             Geometry.Edge edge = Geometry.Edge.FromRhinoLineOrArc2(curve);
 
             // create virtual bar
-            VirtualBar bar = new VirtualBar(edge, edge.CoordinateSystem.LocalY, startConnectivity, endConnectivity, name, ae, itg, i1e, i2e);
+            ModellingTool.VirtualBar bar = new ModellingTool.VirtualBar(edge, edge.CoordinateSystem.LocalY, startConnectivity, endConnectivity, name, ae, itg, i1e, i2e);
 
             // set local y-axis
             if (!v.Equals(Vector3d.Zero))
--- componentsGrasshopper/Deconstruct/CoverDeconstruct.cs
+++ componentsGrasshopper/Deconstruct/CoverDeconstruct.cs
@@ -38,7 +38,7 @@ namespace FemDesign.GH
             // return
             
             DA.SetData(0, cover.Guid);
-            DA.SetData(1, cover.Name);
+            DA.SetData(1, cover.Identifier);
             DA.SetData(2, cover.GetRhinoSurface());
             DA.SetDataList(3, cover.GetRhinoCurves());
        }
