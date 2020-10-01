7c1056916362b4bbcfbf9482d03b5daa37311c11
andosca <andreas.oscarsson@strusoft.com>
2020-10-01 16:20:26 +0200

    Add virtual bar

--- src/AdvancedFem/VirtualBar.cs
+++ src/AdvancedFem/VirtualBar.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
 
 #region dynamo
 using Autodesk.DesignScript.Runtime;
 #endregion
 
-namespace FemDesign
+namespace FemDesign.ModellingTool
 {
     [System.Serializable]
     [IsVisibleInDynamoLibrary(false)]
@@ -160,5 +161,57 @@ namespace FemDesign
             this.I1E = i1e;
             this.I2E = i2e;
         }
+
+        #region dynamo
+        [IsVisibleInDynamoLibrary(true)]
+        /// <summary>
+        /// Create a fictitious bar element.
+        /// </summary>
+        /// <param name="curve">Line or Arc.</param>
+        /// <param name="AE">AE</param>
+        /// <param name="ItG">ItG</param>
+        /// <param name="I1E">I1E</param>
+        /// <param name="I2E">I2E</param>
+        /// <param name="connectivity">Connectivity. If 1 item this item defines both start and end connectivity. If two items the first item defines the start connectivity and the last item defines the end connectivity.</param>
+        /// <param name="localY">LocalY</param>
+        /// <param name="identifier">Identifier. Optional.</param>
+        /// <returns></returns>
+        public static VirtualBar Define(Autodesk.DesignScript.Geometry.Curve curve, double AE, double ItG, double I1E, double I2E, [DefaultArgument("FemDesign.Bars.Connectivity.Default()")] List<Bars.Connectivity> connectivity, [DefaultArgument("Autodesk.DesignScript.Geometry.Vector.ByCoordinates(0,0,0)")] Autodesk.DesignScript.Geometry.Vector localY, [DefaultArgument("BF")] string identifier)
+        {
+            // convert geometry
+            Geometry.Edge edge = Geometry.Edge.FromDynamoLineOrArc2(curve);
+            Geometry.FdVector3d y = Geometry.FdVector3d.FromDynamo(localY);
+
+            // get connectivity
+            Bars.Connectivity startConnectivity;
+            Bars.Connectivity endConnectivity;
+            if (connectivity.Count == 1)
+            {
+                startConnectivity = connectivity[0];
+                endConnectivity = connectivity[0];
+            }
+            else if (connectivity.Count == 2)
+            {
+                startConnectivity = connectivity[0];
+                endConnectivity = connectivity[1];
+            }
+            else
+            {
+                throw new System.ArgumentException($"Connectivity must contain 1 or 2 items. Number of items is {connectivity.Count}");
+            }
+
+            // create virtual bar
+            VirtualBar bar = new VirtualBar(edge, edge.CoordinateSystem.LocalY, startConnectivity, endConnectivity, identifier, AE, ItG, I1E, I2E);
+
+            // set local y-axis
+            if (!localY.Equals(Autodesk.DesignScript.Geometry.Vector.ByCoordinates(0,0,0)))
+            {
+                bar.LocalY = FemDesign.Geometry.FdVector3d.FromDynamo(localY);
+            }
+
+            // return
+            return bar;
+        }
+        #endregion
     }
 }
