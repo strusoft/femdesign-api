0dc594757ecd23f7112739ff368ffa7b75385b35
xRadne <32938476+xRadne@users.noreply.github.com>
2021-05-12 13:52:04 +0200

    :see_no_evil: Include Properties dir with AssemblyInfo

---
+++ FemDesign.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FemDesign API")]
+[assembly: AssemblyDescription("The FEM-Design API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("StruSoft AB")]
+[assembly: AssemblyProduct("FemDesign.Properties")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00f74121-ff13-46da-bd54-1d81c62c4952")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
