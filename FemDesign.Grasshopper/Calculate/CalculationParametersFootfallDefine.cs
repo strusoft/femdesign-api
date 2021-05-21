f75ac1061bd2b598330fe5cf76087dbf4fa8dc1d
xRadne <32938476+xRadne@users.noreply.github.com>
2021-05-21 17:24:52 +0200

    :sparkles: Add Footfall constructors for Grasshopper and dynamo

---
+++ FemDesign.Grasshopper/Calculate/CalculationParametersFootfallDefine.cs
@@ -0,0 +1,48 @@
+// https://strusoft.com/
+using System;
+using Grasshopper.Kernel;
+
+namespace FemDesign.Grasshopper
+{
+    public class CalculationParametersFootfallDefine: GH_Component
+    {
+        public CalculationParametersFootfallDefine(): base("Footfall.Define", "Define", "Define calculation parameters for an footfall calculation.", "FemDesign", "Calculate")
+        {
+
+        }
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Top", "Top", "Top of substructure. Masses on this level and below are not considered in Footfall calculation.", GH_ParamAccess.item, -0.01);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+        }
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Footfall", "Footfall", "Footfall.", GH_ParamAccess.item);
+        }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            double top = -0.01;
+            if (!DA.GetData(0, ref top))
+            {
+                // pass
+            }
+            
+            FemDesign.Calculate.Footfall obj = new Calculate.Footfall(top);
+
+            // return
+            DA.SetData(0, obj);
+        }
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b0a30079-81b6-4a7c-b9b6-3c659a62a13f"); }
+        }
+    }
+}
---
+++ FemDesign.Grasshopper/Loads/FootfallSelfExcitation.cs
@@ -0,0 +1,57 @@
+// https://strusoft.com/
+using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace FemDesign.Grasshopper
+{
+    public class FootfallSelfExcitation : GH_Component
+    {
+        public FootfallSelfExcitation() : base("Footfall.SelfExcitation", "SelfExcitation", "Create a footfall self excitation region.", "FemDesign", "Loads")
+        {
+
+        }
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddSurfaceParameter("Surface", "Surface", "Surface.", GH_ParamAccess.item);
+            pManager.AddTextParameter("Identifier", "Identifier", "Identifier.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddTextParameter("Comment", "Comment", "Comment.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+        }
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Footfall", "Footfall", "Footfall.", GH_ParamAccess.item);
+        }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // get data
+            Brep surface = null;
+            string identifier = null;
+            string comment = null;
+            if (!DA.GetData(0, ref surface)) { return; }
+            if (!DA.GetData(1, ref identifier)) { /* Pass */}
+            if (!DA.GetData(2, ref comment)) { /* Pass */}
+
+            // convert geometry
+            Geometry.Region region = surface.FromRhino();
+
+            // Create a Footfall self excitation
+            var obj = new Loads.Footfall(region, identifier, comment);
+            DA.SetData(0, obj);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("1a637184-84e6-4bdf-8cd6-b96f8c2e3d55"); }
+        }
+    }
+}
