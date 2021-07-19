7c8c778bda066020209cdb8403b857566a9e319f
xRadne <32938476+xRadne@users.noreply.github.com>
2021-07-19 11:49:13 +0200

    Revert ":fire: Node AddConnectedLines/Points removed"
    This reverts commit 5665cd95792e59734ffea214d97ac887448b831b.

--- FemDesign.Dynamo/Dynamo/ModellingTools/ConnectedLines.cs
+++ FemDesign.Dynamo/Dynamo/ModellingTools/ConnectedLines.cs
@@ -10,6 +10,28 @@ namespace FemDesign.ModellingTools
     [IsVisibleInDynamoLibrary(false)]
     public partial class ConnectedLines: EntityBase
     {
-        
+        #region dynamo
+        [IsVisibleInDynamoLibrary(true)]
+        public static ConnectedLines Define(Autodesk.DesignScript.Geometry.Curve firstCurve, Autodesk.DesignScript.Geometry.Curve secondCurve, Autodesk.DesignScript.Geometry.Vector localX, Autodesk.DesignScript.Geometry.Vector localY, Releases.Motions motions, Releases.Rotations rotations, System.Guid[] references, string identifier, bool movingLocal, double interfaceStart, double interfaceEnd)
+        {
+            // convert geometry
+            Geometry.Edge edge0 = Geometry.Edge.FromDynamoLineOrArc2(firstCurve);
+            Geometry.Edge edge1 = Geometry.Edge.FromDynamoLineOrArc2(secondCurve);
+            Geometry.FdVector3d x = Geometry.FdVector3d.FromDynamo(localX);
+            Geometry.FdVector3d y = Geometry.FdVector3d.FromDynamo(localY);
+
+            // rigidity
+            Releases.RigidityDataType3 rigidity = new Releases.RigidityDataType3(motions, rotations);
+
+            // references
+            GuidListType[] refs = new GuidListType[references.Length];
+            for (int idx = 0; idx < refs.Length; idx++)
+            {
+                refs[idx] = new GuidListType(references[idx]);
+            }
+
+            return new ConnectedLines(edge0, edge1, x, y, rigidity, refs, identifier, movingLocal, interfaceStart, interfaceEnd);
+        }
+        #endregion 
     }
 }
--- FemDesign.Dynamo/Dynamo/ModellingTools/ConnectedPoints.cs
+++ FemDesign.Dynamo/Dynamo/ModellingTools/ConnectedPoints.cs
@@ -11,5 +11,28 @@ namespace FemDesign.ModellingTools
     public partial class ConnectedPoints: EntityBase
     {
 
+        #region dynamo
+
+        [IsVisibleInDynamoLibrary(true)]
+        public static ConnectedPoints Define(Autodesk.DesignScript.Geometry.Point firstPoint, Autodesk.DesignScript.Geometry.Point secondPoint, Releases.Motions motions, Releases.Rotations rotations, System.Guid[] references, string identifier)
+        {
+            // convert geometry
+            Geometry.FdPoint3d p1 = Geometry.FdPoint3d.FromDynamo(firstPoint);
+            Geometry.FdPoint3d p2 = Geometry.FdPoint3d.FromDynamo(secondPoint);
+
+            // rigidity
+            Releases.RigidityDataType2 rigidity = new Releases.RigidityDataType2(motions, rotations);
+
+            // references
+            GuidListType[] refs = new GuidListType[references.Length];
+            for (int idx = 0; idx < refs.Length; idx++)
+            {
+                refs[idx] = new GuidListType(references[idx]);
+            }
+
+            return new ConnectedPoints(p1, p2, rigidity, refs, identifier);
+        }
+
+        #endregion 
     }
 }
