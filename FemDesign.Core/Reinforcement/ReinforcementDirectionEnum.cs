57d13344a601d3d86a84b73dba98c9935464fda8
xRadne <32938476+xRadne@users.noreply.github.com>
2021-08-04 16:02:47 +0200

    :memo: ReinforcementDirection comments

--- FemDesign.Core/Reinforcement/ReinforcementDirectionEnum.cs
+++ FemDesign.Core/Reinforcement/ReinforcementDirectionEnum.cs
@@ -8,12 +8,21 @@ using FemDesign.GenericClasses;
 
 namespace FemDesign.Reinforcement
 {
+    /// <summary>
+    /// Reinforcement direction of a reinforcement layout.
+    /// </summary>
     public enum ReinforcementDirection
     {
+        /// <summary>
+        /// X-direction.
+        /// </summary>
         [Parseable("x", "X")]
         [XmlEnum("x")]
         X,
 
+        /// <summary>
+        /// Y-direction
+        /// </summary>
         [Parseable("y", "Y")]
         [XmlEnum("y")]
         Y,
