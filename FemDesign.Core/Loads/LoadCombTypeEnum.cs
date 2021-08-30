361b5dd6f044441e34314c2243c2d2f20991ba76
xRadne <32938476+xRadne@users.noreply.github.com>
2021-08-30 10:15:49 +0200

    :art: PascalCase Enums

--- FemDesign.Core/Loads/LoadCombTypeEnum.cs
+++ FemDesign.Core/Loads/LoadCombTypeEnum.cs
@@ -16,43 +16,43 @@ namespace FemDesign.Loads
         /// <summary>
         /// Ultimate Ordinary, "U", load combination type.
         /// </summary>
-        [Parseable("u", "U", "ultimate_ordinary", "Ultimate_ordinary", "ULTIMATE_ORDINARY")]
+        [Parseable("u", "U", "ultimate_ordinary", "Ultimate_ordinary", "ULTIMATE_ORDINARY", "UltimateOrdinary")]
         [XmlEnum("ultimate_ordinary")]
-        ULTIMATE_ORDINARY,
+        UltimateOrdinary,
 
         /// <summary>
         /// Ultimate accidental, "Ua", load combination type.
         /// </summary>
-        [Parseable("ua", "Ua", "ultimate_accidental", "Ultimate_accidental", "ULTIMATE_ACCIDENTAL")]
+        [Parseable("ua", "Ua", "ultimate_accidental", "Ultimate_accidental", "ULTIMATE_ACCIDENTAL", "UltimateAccidental")]
         [XmlEnum("ultimate_accidental")]
-        ULTIMATE_ACCIDENTAL,
+        UltimateAccidental,
 
         /// <summary>
         /// Ultimate seismic, "Us", load combination type.
         /// </summary>
-        [Parseable("us", "Us", "ultimate_seismic", "Ultimate_seismic", "ULTIMATE_SEISMIC")]
+        [Parseable("us", "Us", "ultimate_seismic", "Ultimate_seismic", "ULTIMATE_SEISMIC", "UltimateSeismic")]
         [XmlEnum("ultimate_seismic")]
-        ULTIMATE_SEISMIC,
+        UltimateSeismic,
 
         /// <summary>
         /// Serviceability quasi permanent, "Sq", load combination type.
         /// </summary>
-        [Parseable("sq", "Sq", "serviceability_quasi_permanent", "Serviceability_quasi_permanent", "SERVICEABILITY_QUASI_PERMANENT")]
+        [Parseable("sq", "Sq", "serviceability_quasi_permanent", "Serviceability_quasi_permanent", "SERVICEABILITY_QUASI_PERMANENT", "ServicabilityQuasiPermanent")]
         [XmlEnum("serviceability_quasi_permanent")]
-        SERVICEABILITY_QUASI_PERMANENT,
+        ServicabilityQuasiPermanent,
 
         /// <summary>
         /// Serviceability frequent, "Sf", load combination type.
         /// </summary>
-        [Parseable("sf", "Sf", "serviceability_frequent", "Serviceability_frequent", "SERVICEABILITY_FREQUENT")]
+        [Parseable("sf", "Sf", "serviceability_frequent", "Serviceability_frequent", "SERVICEABILITY_FREQUENT", "ServicabilityFrequent")]
         [XmlEnum("serviceability_frequent")]
-        SERVICEABILITY_FREQUENT,
+        ServicabilityFrequent,
 
         /// <summary>
         /// Serviceability characteristic, "Sc", load combination type.
         /// </summary>
-        [Parseable("sc", "Sc", "serviceability_characteristic", "Serviceability_characteristic", "SERVICEABILITY_CHARACTERISTIC")]
+        [Parseable("sc", "Sc", "serviceability_characteristic", "Serviceability_characteristic", "SERVICEABILITY_CHARACTERISTIC", "ServiceabilityCharacteristic")]
         [XmlEnum("serviceability_characteristic")]
-        SERVICEABILITY_CHARACTERISTIC,
+        ServiceabilityCharacteristic,
     }
 }
