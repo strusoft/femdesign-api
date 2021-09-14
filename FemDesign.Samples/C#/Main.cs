a8f9a8a07ce4287bd541817512f2461c2613af13
JRiad <89396751+JRiad@users.noreply.github.com>
2021-09-14 16:06:05 +0200

    Added examples C#

---
+++ FemDesign.Samples/C#/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using FemDesign;
+
+
+namespace FemDesign.Samples
+{
+    public partial class SampleProgram
+    {
+        private static void Main(string[] args)
+        {
+            string samplesDir = System.IO.Directory.GetCurrentDirectory();
+            if (samplesDir.EndsWith("bin\\Debug\\net5.0") || samplesDir.EndsWith("bin\\Release\\net5.0"))
+                samplesDir = Path.GetFullPath(Path.Combine(samplesDir, "..\\..\\..\\"));
+            System.IO.Directory.SetCurrentDirectory(samplesDir);
+
+            Console.WriteLine("Running some sample code using the FEM-Design api!");
+
+            //TestGetResults();
+            ChangeLengthOfBeam();
+            //RunAnalysis();
+        }
+    }
+}
