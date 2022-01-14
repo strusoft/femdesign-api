97426171576f1c72464aefaa2b5180eb08cef040
Andreas Oscarsson <52665994+andosca@users.noreply.github.com>
2022-01-14 16:26:44 +0100

    change assembly version

--- FemDesign.Core/Properties/AssemblyInfo.cs
+++ FemDesign.Core/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("20.2.0.0")]
-[assembly: AssemblyFileVersion("20.2.0.0")]
+[assembly: AssemblyVersion("21.0.0.0")]
+[assembly: AssemblyFileVersion("21.0.0.0")]
