cf3ce67e0164221d00d78b07d1e765cfa373c89f
Alexander Radne <32938476+xRadne@users.noreply.github.com>
2022-02-11 09:39:13 +0100

    Change version

--- FemDesign.Core/Properties/GlobalAssemblyInfo.cs
+++ FemDesign.Core/Properties/GlobalAssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("21.0.0.0")]
-[assembly: AssemblyFileVersion("21.0.0.0")]
+[assembly: AssemblyVersion("21.0.1.0")]
+[assembly: AssemblyFileVersion("21.0.1.0")]
