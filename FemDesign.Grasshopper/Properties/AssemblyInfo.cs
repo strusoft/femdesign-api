7db4ba22b2ad89c0604b8c56146b39a48fc0ab03
xRadne <32938476+xRadne@users.noreply.github.com>
2022-02-14 17:15:31 +0100

    :arrow_up: Upgrade to 21.1.0
    New feature: Load groups

--- FemDesign.Grasshopper/Properties/AssemblyInfo.cs
+++ FemDesign.Grasshopper/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("21.0.1.0")]
-[assembly: AssemblyFileVersion("21.0.1.0")]
+[assembly: AssemblyVersion("21.1.0.0")]
+[assembly: AssemblyFileVersion("21.1.0.0")]
