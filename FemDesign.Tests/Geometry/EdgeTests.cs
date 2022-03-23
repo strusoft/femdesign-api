d4f0b4d489ddac73e75294165363d8160ff259c4
xRadne <32938476+xRadne@users.noreply.github.com>
2022-03-23 13:41:08 +0100

    :zap: Split Edge.GetLength tests to separate tests

--- FemDesign.Tests/Geometry/EdgeTests.cs
+++ FemDesign.Tests/Geometry/EdgeTests.cs
@@ -6,8 +6,8 @@ namespace FemDesign.Geometry.Tests
     [TestClass()]
     public class EdgeTests
     {
-        [TestMethod("GetLength")]
-        public void GetLength()
+        [TestMethod("GetLength Arc")]
+        public void GetLengthArc()
         {
             // arc by 
             var center = new FdPoint3d(0,0,0);
@@ -18,27 +18,51 @@ namespace FemDesign.Geometry.Tests
             var cs = FdCoordinateSystem.Global();
             var arc1 = new Edge(radius, startAngle, endAngle, center, xAxis, cs);
             Assert.IsTrue(Math.Abs(arc1.Length - Math.PI) <= Tolerance.LengthComparison);
+        }
 
+        [TestMethod("GetLength Circle")]
+        public void GetLengthCircle()
+        {
             // circle
+            var center = new FdPoint3d(0, 0, 0);
+            var radius = 1;
+            var cs = FdCoordinateSystem.Global();
             var circle = new Edge(radius, center, cs);
             Assert.IsTrue(Math.Abs(circle.Length - 2 * Math.PI) <= Tolerance.LengthComparison);
+        }
 
+        [TestMethod("GetLength Line")]
+        public void GetLengthLine()
+        {
             // line
-            var p0 = new FdPoint3d(0,0,0);
-            var p1 = new FdPoint3d(1,0,0);
-            var y = new FdVector3d(0,1,0);
+            var p0 = new FdPoint3d(0, 0, 0);
+            var p1 = new FdPoint3d(1, 0, 0);
+            var y = new FdVector3d(0, 1, 0);
             var line = new Edge(p0, p1, y);
             Assert.IsTrue(Math.Abs(line.Length - 1) <= Tolerance.LengthComparison);
+        }
 
+        [TestMethod("GetLength Invalid")]
+        public void GetLengthInvalid()
+        {
             // some other type
+            var p0 = new FdPoint3d(0, 0, 0);
+            var p1 = new FdPoint3d(1, 0, 0);
+            var y = new FdVector3d(0, 1, 0);
+            var line = new Edge(p0, p1, y);
             var edge = line;
             edge._type = "someType";
             Assert.ThrowsException<ArgumentException>(() => edge.Length, "Should throw exception on not supported edge type");
-            
+        }
+
+        [TestMethod("GetLength Arc 3 point")]
+        public void GetLengthArc3Point()
+        {
             // arc by three points
-            var start = new FdPoint3d(-1,0,0);
-            var mid = new FdPoint3d(0,1,0);
-            var end = new FdPoint3d(1,0,0);
+            var start = new FdPoint3d(-1, 0, 0);
+            var mid = new FdPoint3d(0, 1, 0);
+            var end = new FdPoint3d(1, 0, 0);
+            var cs = FdCoordinateSystem.Global();
             var arc2 = new Edge(start, mid, end, cs);
             //Assert.ThrowsException<ArgumentException>(() => arc2.Length, "Should throw exception stating that method to calculate sweep angle is not implemented yet.");
             Assert.IsTrue(Math.Abs(arc2.Length - Math.PI) <= Tolerance.LengthComparison); // test should fail here 
