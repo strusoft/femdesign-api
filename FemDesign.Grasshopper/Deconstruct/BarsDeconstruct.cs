9415d51477d9a21dd6f17fbffc7781f98e22860f
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-04-01 10:39:33 +0200

    datatree output for deconstruct bar

--- FemDesign.Grasshopper/Deconstruct/BarsDeconstruct.cs
+++ FemDesign.Grasshopper/Deconstruct/BarsDeconstruct.cs
@@ -1,7 +1,9 @@
 // https://strusoft.com/
 using System;
 using System.Collections.Generic;
+using Grasshopper;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
 using Rhino.Geometry;
 
 namespace FemDesign.Grasshopper
@@ -18,18 +20,18 @@ namespace FemDesign.Grasshopper
        } 
        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
        {
-           pManager.AddTextParameter("Guid", "Guid", "Guid.", GH_ParamAccess.item);
-           pManager.AddCurveParameter("Curve", "Curve", "LineCurve or ArcCurve", GH_ParamAccess.item);
-           pManager.AddGenericParameter("Type", "Type", "Bar type", GH_ParamAccess.item);
-           pManager.AddGenericParameter("Material", "Material", "Material", GH_ParamAccess.item);
+           pManager.AddTextParameter("Guid", "Guid", "Guid.", GH_ParamAccess.list);
+           pManager.AddCurveParameter("Curve", "Curve", "LineCurve or ArcCurve", GH_ParamAccess.list);
+           pManager.AddGenericParameter("Type", "Type", "Bar type", GH_ParamAccess.list);
+           pManager.AddGenericParameter("Material", "Material", "Material", GH_ParamAccess.list);
            pManager.AddGenericParameter("Section", "Section", "Section", GH_ParamAccess.list);
            pManager.AddGenericParameter("Connectivity", "Connectivity", "Connectivity", GH_ParamAccess.list);
            pManager.AddGenericParameter("Eccentricity", "Eccentricity", "Eccentricity", GH_ParamAccess.list);
-           pManager.AddGenericParameter("LocalY", "LocalY", "LocalY", GH_ParamAccess.item);
+           pManager.AddGenericParameter("LocalY", "LocalY", "LocalY", GH_ParamAccess.list);
            pManager.AddGenericParameter("Stirrups", "Stirrups", "Stirrups.", GH_ParamAccess.list);
            pManager.AddGenericParameter("LongitudinalBars", "LongBars", "Longitudinal bars.", GH_ParamAccess.list);
            pManager.AddGenericParameter("PTC", "PTC", "Post-tensioning cables.", GH_ParamAccess.list);
-           pManager.AddTextParameter("Identifier", "Identifier", "Structural element ID.", GH_ParamAccess.item);
+           pManager.AddTextParameter("Identifier", "Identifier", "Structural element ID.", GH_ParamAccess.list);
        }
        protected override void SolveInstance(IGH_DataAccess DA)
        {
@@ -44,19 +46,41 @@ namespace FemDesign.Grasshopper
                 return;
             }
 
+
+
+            // The following code is to convert 'item' to 'list object'
+            // It is required to construct the bar without graftening the data
+
+            List<object> guidList = new List<object>();
+            guidList.Add(bar.Guid);
+
+            List<object> curveList = new List<object>();
+            curveList.Add(bar.GetRhinoCurve());
+
+            List<object> typeList = new List<object>();
+            typeList.Add(bar.Type);
+
+            List<object> materialList = new List<object>();
+            materialList.Add(bar.BarPart.Material);
+
+            List<object> localYList = new List<object>();
+            localYList.Add(bar.BarPart.LocalY.ToRhino());
+
+
             // return
-            DA.SetData(0, bar.Guid);
-            DA.SetData(1, bar.GetRhinoCurve());
-            DA.SetData(2, bar.Type);
-            DA.SetData(3, bar.BarPart.Material);
+            DA.SetDataList(0, guidList);
+            DA.SetDataList(1, curveList);
+            DA.SetDataList(2, typeList);
+            DA.SetDataList(3, materialList);
             DA.SetDataList(4, bar.BarPart.Sections);
             DA.SetDataList(5, bar.BarPart.Connectivities);
             DA.SetDataList(6, bar.BarPart.Eccentricities);
-            DA.SetData(7, bar.BarPart.LocalY.ToRhino());
+            DA.SetDataList(7, localYList);
             DA.SetDataList(8, bar.Stirrups);
             DA.SetDataList(9, bar.LongitudinalBars);
             DA.SetDataList(10, bar.Ptc);
-            DA.SetData(11, bar.Identifier);
+            DA.SetDataList(11, bar.Identifier);
+            
        }
        protected override System.Drawing.Bitmap Icon
        {
