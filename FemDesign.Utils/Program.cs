81f0b32097a3f9e4819b8923beba9568658eb4eb
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-04-04 18:15:21 +0200

    public methods for Revit are now hidden.
    create a script to generate an FD_21.00.001.cs to add the [IsVisibleInDynamoLibrary(false)] attribute to the public classes

---
+++ FemDesign.Utils/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using System;
+using System.Linq;
+
+
+var myAssembly = Assembly.GetAssembly(typeof(FemDesign.Model));
+var nameSpace = "StruSoft.Interop.StruXml.Data";
+
+var typeList = myAssembly.GetTypes()
+          .Where(t => string.Equals(t.Namespace, nameSpace, StringComparison.Ordinal) && t.IsEnum == false)
+          .ToArray();
+
+var enumType = myAssembly.GetTypes()
+          .Where(t => string.Equals(t.Namespace, nameSpace, StringComparison.Ordinal) && t.IsEnum == true)
+          .ToArray();
+
+
+string template =    "using System.Collections.Generic;\n" +
+                     "using System.Xml.Serialization;\n\n" +
+                     "#region dynamo\n" +
+                     "using Autodesk.DesignScript.Runtime;\n" +
+                     "#endregion\n\n" +
+                     "namespace StruSoft.Interop.StruXml.Data\n{";
+
+// Generate a C# class programmaticall
+// adding [IsVisibleInDynamoLibrary(false)] to all the partial class
+// for public enum, there is no solution yet
+
+for (int i = 0; i < typeList.Length; i++)
+{
+    string attribute = "[IsVisibleInDynamoLibrary(false)]\n";
+    string className = string.Format("public partial class {0}\n", typeList[i].Name);
+    string bracket = "{\n}\n\n";
+    template += attribute + className + bracket;
+}
+
+template += "}";
+
+// Find the Current Directory and look for the right folder where the modified FD_21.00.001.cs needs to be saved
+string currentDirectory = Directory.GetCurrentDirectory();
+
+var targetDirectoryNames = currentDirectory.Split(Path.DirectorySeparatorChar);
+int folderLength = targetDirectoryNames.Length;
+
+var targetDirectory = Path.Join(targetDirectoryNames[..(folderLength-4)]);
+
+// FD name should reference a filename and not manually type
+string fileName = "FD_21.00.001.cs";
+
+string fileOutput = Path.Join(targetDirectory + @"\FemDesign.Dynamo\Dynamo\StruSoft\Interop\StruXml\Data", fileName);
+
+Console.WriteLine(fileOutput);
+
+await File.WriteAllTextAsync(fileOutput, template);
