75819ab8dad7922b4bfe086207117765a389691b
Samuel Nyberg <samuel.nyberg@strusoft.com>
2022-04-06 11:01:37 +0200

    #293 - first, shaky steps

--- FemDesign.Samples/C#/Main.cs
+++ FemDesign.Samples/C#/Main.cs
@@ -12,12 +12,12 @@ namespace FemDesign.Samples
         /// <summary>
         /// NOTE: CHANGE THIS TO YOUR LOCAL MATERIALS
         /// </summary>
-        static readonly string MaterialsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "StruSoft", "FEM-Design Api", "materialLibrary", "materials_S.struxml");
+        static readonly string MaterialsPath = @"C:\Users\SamuelNyberg\OneDrive - StruSoft AB\Samuels arbetshörna\materialochsektioner\materials.struxml";
 
         /// <summary>
         /// NOTE: CHANGE THIS TO YOUR LOCAL SECTIONS
         /// </summary>
-        static readonly string SectionsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "StruSoft", "FEM-Design Api", "sectionLibrary", "sections.struxml");
+        static readonly string SectionsPath = @"C:\Users\SamuelNyberg\OneDrive - StruSoft AB\Samuels arbetshörna\materialochsektioner\sections.struxml";
 
         private static void Main(string[] args)
         {
@@ -29,12 +29,13 @@ namespace FemDesign.Samples
             Console.WriteLine("Running some sample code using the FEM-Design api!");
 
             // ChangeLengthOfBeam();
-            // CreateSimpleModel();
+            // Example1CreateSimpleModel();
+            Example4EditExistingModel();
             // RunAnalysis();
             // CreatePostTensionedCable();
             // CreateLoadGroups();
             // LoadGroupsCombine();
-            ParametricStudy();
+            // ParametricStudy();
             // CostOptimizationOfSlab();
         }
     }
