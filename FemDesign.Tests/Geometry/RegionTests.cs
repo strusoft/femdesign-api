25eda0d8f1b68e9ae568075855ecef0cba2ef292
xRadne <32938476+xRadne@users.noreply.github.com>
2022-04-13 10:18:53 +0200

    :bug: Fix serialization of custom edge connections
    Fix custom edge connections was not serialized properly (were previously nulled)
    Fix #303

--- FemDesign.Tests/Geometry/RegionTests.cs
+++ FemDesign.Tests/Geometry/RegionTests.cs
@@ -73,7 +73,7 @@ namespace FemDesign.Geometry.Tests
             Assert.IsNotNull(edgeConnections[3], $"Edge connection 3 should not be null");
         }
 
-        [TestMethod("SetEdgeConnection - On slab")]
+        [TestMethod("SetEdgeConnection - Set hinge on slab")]
         public void SetEdgeConnectionTest4()
         {
             string input = "Geometry/Slabs.struxml";
@@ -91,6 +91,26 @@ namespace FemDesign.Geometry.Tests
             Assert.IsTrue(newSlab.SlabPart.GetEdgeConnections().Where(ec => !(ec is null)).Count() == 2, "Should have edge connections");
             Assert.IsNotNull(newSlab.SlabPart.GetEdgeConnections()[1], "Should have edge connection");
             Assert.IsNotNull(newSlab.SlabPart.GetEdgeConnections()[3], "Should have edge connection");
+        }
+
+        [TestMethod("SetEdgeConnection - Set rigid on slab")]
+        public void SetEdgeConnectionTest8()
+        {
+            string input = "Geometry/Slabs.struxml";
+            Model model = Model.DeserializeFromFilePath(input);
+            var slab = model.Entities.Slabs.First();
+
+            var before = slab.SlabPart.GetEdgeConnections();
+            foreach (var ec in before)
+                Assert.IsNull(ec);
+
+
+            // Set the edge connections
+            var newSlab = Shells.Slab.ShellEdgeConnection(slab, Shells.ShellEdgeConnection.GetRigid(), new List<int> { 1, 3 });
+
+            Assert.IsNull(newSlab.SlabPart.GetEdgeConnections()[1], "Should be null if the edge connection is default rigid");
+            Assert.IsNull(newSlab.SlabPart.GetEdgeConnections()[3], "Should be null if the edge connection is default rigid");
+            Assert.IsTrue(newSlab.SlabPart.GetEdgeConnections().Where(ec => !(ec is null)).Count() == 0, "Should have edge connections");
         }
 
         [TestMethod("SetEdgeConnection - Add to model and overwrite")]
@@ -137,6 +157,41 @@ namespace FemDesign.Geometry.Tests
 
             model.AddElements(new List<GenericClasses.IStructureElement> { newSlab });
 
+            model.SerializeModel(output);
+        }
+
+        [TestMethod("SetEdgeConnection - deserialize")]
+        public void SetEdgeConnectionTest7()
+        {
+            string input = "Geometry/Slabs.struxml";
+            Model model = Model.DeserializeFromFilePath(input);
+            var slab1 = model.Entities.Slabs[0];
+            var slab2 = model.Entities.Slabs[1];
+
+            var ec1 = slab1.SlabPart.GetEdgeConnections();
+            var ec2 = slab2.SlabPart.GetEdgeConnections();
+
+            Assert.IsTrue(ec1.Count == 4);
+            Assert.IsNull(ec1[0]);
+            Assert.IsNull(ec1[1]);
+            Assert.IsNull(ec1[2]);
+            Assert.IsNull(ec1[3]);
+
+            Assert.IsNull(ec2[0]);
+            Assert.IsNotNull(ec2[1]);
+            Assert.IsTrue(ec2[1].Rigidity.Motions.XNeg == 0.0);
+            Assert.IsNotNull(ec2[2]);
+            Assert.IsTrue(ec2[2].Rigidity.Motions.XNeg == 10000000);
+            Assert.IsNotNull(ec2[3]);
+            Assert.IsTrue(ec2[3].Rigidity.Motions.XNeg == 10000000);
+        }
+
+        [TestMethod("SetEdgeConnection - Deserialize-serialize")]
+        public void SetEdgeConnectionTest9()
+        {
+            string input = "Geometry/Slabs.struxml";
+            string output = "Geometry/Slabs-deserialize-serialize.struxml";
+            Model model = Model.DeserializeFromFilePath(input);
             model.SerializeModel(output);
         }
     }
