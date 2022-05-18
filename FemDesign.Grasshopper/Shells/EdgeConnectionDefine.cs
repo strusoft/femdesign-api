d34376c59e929c072196bef57eda35fdd6bf65ca
xRadne <32938476+xRadne@users.noreply.github.com>
2022-05-18 13:52:54 +0200

    🦗 EdgeConnection.Define now use LibraryName
    - Updated EdgeConnection.Define
    - Marked old component as obsolete
    
    Fix #365

--- FemDesign.Grasshopper/Shells/EdgeConnectionDefine.cs
+++ FemDesign.Grasshopper/Shells/EdgeConnectionDefine.cs
@@ -18,6 +18,8 @@ namespace FemDesign.Grasshopper
             pManager[pManager.ParamCount - 1].Optional = true;
             pManager.AddGenericParameter("Plastic Limits Moments Rotations", "PlaLimR", "Plastic limits moments for rotation springs. Optional.", GH_ParamAccess.item);
             pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddTextParameter("LibraryName", "LibraryName", "When libraryName is not null or empty, the edge connection will be treated as a \"predefined/library\" item. Default is to treat is a a unique \"custom\" edge connection.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
         }
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
@@ -29,17 +31,19 @@ namespace FemDesign.Grasshopper
             Releases.Rotations rotations = null;
             Releases.MotionsPlasticLimits motionsPlasticLimit = null;
             Releases.RotationsPlasticLimits rotationsPlasticLimit = null;
+            string libraryName = null;
             if (!DA.GetData("Motions", ref motions)) return;
             if (!DA.GetData("Rotations", ref rotations)) return;
             if (motions == null || rotations == null) return;
             DA.GetData("Plastic Limits Forces Motions", ref motionsPlasticLimit);
             DA.GetData("Plastic Limits Moments Rotations", ref rotationsPlasticLimit);
+            DA.GetData("LibraryName", ref libraryName);
 
-            Shells.EdgeConnection edgeConnection = new Shells.EdgeConnection(motions, motionsPlasticLimit, rotations, rotationsPlasticLimit);
+            Shells.EdgeConnection edgeConnection = new Shells.EdgeConnection(motions, motionsPlasticLimit, rotations, rotationsPlasticLimit, libraryName);
 
             DA.SetData("EdgeConnection", edgeConnection);
         }
         protected override System.Drawing.Bitmap Icon => FemDesign.Properties.Resources.EdgeConnectionDefine;
-        public override Guid ComponentGuid => new Guid("c6088f65-a1ca-4c37-9bca-7f5ef3d41e70");
+        public override Guid ComponentGuid => new Guid("d4748927-2190-4444-82a0-82df4593eca6");
     }
 }
