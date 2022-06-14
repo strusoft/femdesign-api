7af7485911797a575c4aa24a7299a53f2f46943d
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-06-14 16:43:58 +0200

    Update Entities.cs

--- FemDesign.Core/Model/Entities.cs
+++ FemDesign.Core/Model/Entities.cs
@@ -15,66 +15,69 @@ namespace FemDesign
         // if dummy elements are not used for undefined types deserialization will not work properly
         // when serializing these dummy elements must be nulled. 
         [XmlElement("foundations", Order = 1)]
-        public List<StruSoft.Interop.StruXml.Data.Foundation_type> Foundations { get; set; } 
+        public List<StruSoft.Interop.StruXml.Data.Foundation_type> Foundations { get; set; }
 
-        [XmlElement("bar", Order = 2)]
+        [XmlElement("soil_elements", Order = 2)]
+        public StruSoft.Interop.StruXml.Data.DatabaseEntitiesSoil_elements SoilElements { get; set; }
+
+        [XmlElement("bar", Order = 3)]
         public List<Bars.Bar> Bars { get; set; } = new List<Bars.Bar>();
 
-        [XmlElement("column_corbel", Order = 3)]
+        [XmlElement("column_corbel", Order = 4)]
         public List<Bars.ColumnCorbel> ColumnCorbels { get; set; } = new List<Bars.ColumnCorbel>();
 
-        [XmlElement("hidden_bar", Order = 4)]
+        [XmlElement("hidden_bar", Order = 5)]
         public List<Reinforcement.HiddenBar> HiddenBars { get; set; } = new List<Reinforcement.HiddenBar>();
 
-        [XmlElement("bar_reinforcement", Order = 5)]
+        [XmlElement("bar_reinforcement", Order = 6)]
         public List<Reinforcement.BarReinforcement> BarReinforcements { get; set; } = new List<Reinforcement.BarReinforcement>();
 
-        [XmlElement("slab", Order = 6)]
+        [XmlElement("slab", Order = 7)]
         public List<Shells.Slab> Slabs { get; set; } = new List<Shells.Slab>();
 
-        [XmlElement("shell_buckling", Order = 7)]
+        [XmlElement("shell_buckling", Order = 8)]
         public List<Shells.ShellBucklingType> ShellBucklings { get; set; } = new List<Shells.ShellBucklingType>();
 
-        [XmlElement("wall_corbel", Order = 8)]
+        [XmlElement("wall_corbel", Order = 9)]
         public List<Shells.WallCorbel> WallCorbel { get; set; } = new List<Shells.WallCorbel>();
 
-        [XmlElement("surface_reinforcement_parameters", Order = 9)]
+        [XmlElement("surface_reinforcement_parameters", Order = 10)]
         public List<Reinforcement.SurfaceReinforcementParameters> SurfaceReinforcementParameters { get; set; } = new List<Reinforcement.SurfaceReinforcementParameters>();
 
-        [XmlElement("surface_reinforcement", Order = 10)]
+        [XmlElement("surface_reinforcement", Order = 11)]
         public List<Reinforcement.SurfaceReinforcement> SurfaceReinforcements { get; set; } = new List<Reinforcement.SurfaceReinforcement>();
 
-        [XmlElement("punching_area", Order = 11)]
+        [XmlElement("punching_area", Order = 12)]
         public List<Reinforcement.PunchingArea> PunchingArea { get; set; } = new List<Reinforcement.PunchingArea>();
 
-        [XmlElement("punching_reinforcement", Order = 12)]
+        [XmlElement("punching_reinforcement", Order = 13)]
         public List<Reinforcement.PunchingReinforcement> PunchingReinforcements { get; set; } = new List<Reinforcement.PunchingReinforcement>();
 
-        [XmlElement("no-shear_region", Order = 13)]
+        [XmlElement("no-shear_region", Order = 14)]
         public List<Reinforcement.NoShearRegionType> NoShearRegions { get; set; } = new List<Reinforcement.NoShearRegionType>();
 
-        [XmlElement("panel", Order = 14)]
+        [XmlElement("panel", Order = 15)]
         public List<Shells.Panel> Panels { get; set; } = new List<Shells.Panel>();
 
-        [XmlElement("post-tensioned_cable", Order = 15)]
+        [XmlElement("post-tensioned_cable", Order = 16)]
         public List<Reinforcement.Ptc> PostTensionedCables { get; set; } = new List<Reinforcement.Ptc>();
 
-        [XmlElement("loads", Order = 16)]
+        [XmlElement("loads", Order = 17)]
         public Loads.Loads Loads { get; set; } = new Loads.Loads();
 
-        [XmlElement("supports", Order = 17)]
+        [XmlElement("supports", Order = 18)]
         public Supports.Supports Supports { get; set; } = new Supports.Supports();
 
-        [XmlElement("advanced-fem", Order = 18)]
+        [XmlElement("advanced-fem", Order = 19)]
         public AdvancedFem AdvancedFem { get; set; } = new AdvancedFem();
 
-        [XmlElement("storeys", Order = 19)]
+        [XmlElement("storeys", Order = 20)]
         public StructureGrid.Storeys Storeys { get; set; }
 
-        [XmlElement("axes", Order = 20)]
+        [XmlElement("axes", Order = 21)]
         public StructureGrid.Axes Axes { get; set; }
 
-        [XmlElement("labelled_sections_geometry", Order = 21)]
+        [XmlElement("labelled_sections_geometry", Order = 22)]
         public AuxiliaryResults.LabelledSectionsGeometry LabelledSections;
 
         // axes
