d2a0504c4a2c91a848a6808d42b9525d4faf6cb0
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-07-20 16:38:01 +0200

    :memo: LoadCase type

--- FemDesign.Core/Loads/LoadCaseTypeEnum.cs
+++ FemDesign.Core/Loads/LoadCaseTypeEnum.cs
@@ -16,7 +16,7 @@ namespace FemDesign.Loads
         /// <summary>
         /// "Ordinary" load type
         /// </summary>
-        [Parseable("Ordinary", "static", "Static", "STATIC")]
+        [Parseable("ordinary", "Ordinary", "ORDINARY","static", "Static", "STATIC")]
         [XmlEnum("static")]
         Static,
 
@@ -44,7 +44,7 @@ namespace FemDesign.Loads
         /// <summary>
         /// "Camber sim." load type
         /// </summary>
-        [Parseable("+Shrinkage", "prestressing", "Prestressing", "PRESTRESSING", "camber_sim", "Camber_sim", "CAMBER_SIM")]
+        [Parseable("+Shrinkage", "Shrinkage", "shrinkage", "Shrinkage", "SHRINKAGE", "Prestressing", "PRESTRESSING", "camber_sim", "Camber_sim", "CAMBER_SIM")]
         [XmlEnum("prestressing")]
         Prestressing,
 
