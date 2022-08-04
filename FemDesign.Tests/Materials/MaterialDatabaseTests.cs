0f80f43c903a9026442aa2a6f3c50d320de1a8b9
xRadne <32938476+xRadne@users.noreply.github.com>
2022-08-04 15:36:41 +0200

    :zap: MaterialDatabase.GetDefault performace test
    Also fixes #272

--- FemDesign.Tests/Materials/MaterialDatabaseTests.cs
+++ FemDesign.Tests/Materials/MaterialDatabaseTests.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FemDesign.Sections;
+using FemDesign.Materials;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,25 +7,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
-namespace FemDesign.Sections.Tests
+namespace FemDesign.Materials.Tests
 {
     [TestClass()]
-    public class SectionDatabaseTests
+    public class MaterialDatabaseTests
     {
-        [TestMethod("SectionDatabase.GetDefault - Speed")]
+        [TestMethod("MaterialDatabase.GetDefault - Speed")]
         [TestCategory("Performance")]
         public void GetDefaultTest()
         {
             Stopwatch sw = new Stopwatch();
 
             sw.Start();
-            SectionDatabase.GetDefault();
+            MaterialDatabase.GetDefault();
             sw.Stop();
             Assert.IsTrue(sw.ElapsedMilliseconds < 500, $"GetDefault once should be fast. {sw.ElapsedMilliseconds}");
 
             sw.Restart();
             for (int i = 0; i < 10; i++)
-                SectionDatabase.GetDefault();
+                MaterialDatabase.GetDefault();
             sw.Stop();
             Assert.IsTrue(sw.ElapsedMilliseconds < 100, $"GetDefault many times after the first should be very fast. {sw.ElapsedMilliseconds}");
         }
