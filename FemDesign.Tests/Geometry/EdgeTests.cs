af68463f855f86c931052cd146133c32e76861e7
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-09-26 16:06:44 +0200

    :sparkles: PointAtLength
    #516

--- FemDesign.Tests/Geometry/EdgeTests.cs
+++ FemDesign.Tests/Geometry/EdgeTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace FemDesign.Geometry.EdgeTests
 {
@@ -77,7 +78,7 @@ namespace FemDesign.Geometry.EdgeTests
 
         [TestMethod("Create-LineEdge")]
         public void EdgeLine()
-		{
+        {
             // line
             var p0 = new Point3d(0, 0, 0);
             var p1 = new Point3d(1, 0, 0);
@@ -107,24 +108,101 @@ namespace FemDesign.Geometry.EdgeTests
         [TestMethod("Create-RadialEdgeLines")]
         public void RadialEdgeLines()
         {
+            foreach(var line in CreateLines())
+			{
+                Assert.IsTrue( line.IsLine() );
+			}
+        }
+
+        [TestMethod("PointAtLength")]
+        public void PointAtLenght()
+        {
+            foreach(var line in CreateLines())
+			{
+                var p0 = line.PointAtLength(0.0);
+                var p1 = line.PointAtLength(0.5);
+                var p2 = line.PointAtLength(1.0);
+
+                Assert.ThrowsException<ArgumentException>(() => line.PointAtLength(20.0));
+                Assert.ThrowsException<ArgumentException>(() => line.PointAtLength(-5.0));
+            }
+
+            
+            var p3 = new Point3d(0, 0, -1);
+            var p4 = new Point3d(12, 0, 4);
+
+            var lineEdge = new LineEdge(p3, p4);
+            var pointOncurve = lineEdge.PointAtLength(1.0);
+
+            // Values taken from Rhino Method
+            Assert.AreEqual(pointOncurve.X, 0.923077, 0.01);
+            Assert.AreEqual(pointOncurve.Y, 0.00, 0.01);
+            Assert.AreEqual(pointOncurve.Z, -0.615385, 0.01);
+
+            lineEdge = new LineEdge(p3, p4 * -1);
+            pointOncurve = lineEdge.PointAtLength(1.0);
+
+            // Value taken from Rhino Method
+            Assert.AreEqual(pointOncurve.X, -0.970143, 0.01);
+            Assert.AreEqual(pointOncurve.Y, 0.00, 0.01);
+            Assert.AreEqual(pointOncurve.Z, -1.242536, 0.01);
+
+            p3 = new Point3d(0, 0, 0);
+            p4 = new Point3d(10, 0, 0);
+
+            lineEdge = new LineEdge(p3, p4);
+            pointOncurve = lineEdge.PointAtLength(1.0);
+
+            // Value taken from Rhino Method
+            Assert.AreEqual(pointOncurve.X, 1.00, 0.01);
+            Assert.AreEqual(pointOncurve.Y, 0.00, 0.01);
+            Assert.AreEqual(pointOncurve.Z, 0.00, 0.01);
+
+
+            p3 = new Point3d(0, 0, -5);
+            p4 = new Point3d(0, 0, 5);
+
+            lineEdge = new LineEdge(p3, p4);
+            pointOncurve = lineEdge.PointAtLength(5.0);
+
+            // Value taken from Rhino Method
+            Assert.AreEqual(pointOncurve.X, 0.00, 0.01);
+            Assert.AreEqual(pointOncurve.Y, 0.00, 0.01);
+            Assert.AreEqual(pointOncurve.Z, 0.00, 0.01);
+
+            pointOncurve = lineEdge.PointAtLength(1.0);
+
+            // Value taken from Rhino Method
+            Assert.AreEqual(pointOncurve.X, 0.00, 0.01);
+            Assert.AreEqual(pointOncurve.Y, 0.00, 0.01);
+            Assert.AreEqual(pointOncurve.Z, -4.00, 0.01);
+
+        }
+
+        private List<LineEdge> CreateLines()
+        {
             // line
+            var edgeLines = new List<LineEdge>();
+            var edges = new List<Edge>();
             var p0 = new Point3d(0, 0, 0);
 
-            for(int i = -1; i < 1; i++)
-			{
-                for(int j = -1; j < 1; j++)
-				{
-                    for(int k = -1; k < 1; k++)
-					{
+            for (int i = -1; i < 1; i++)
+            {
+                for (int j = -1; j < 1; j++)
+                {
+                    for (int k = -1; k < 1; k++)
+                    {
                         if (i == 0 && j == 0 && k == 0) continue;
                         var p1 = new Point3d(i, j, k);
-                        var edge = new FemDesign.Geometry.Edge(p0, p1);
                         var edgeLine = new FemDesign.Geometry.LineEdge(p0, p1);
+                        var edge = new FemDesign.Geometry.Edge(p0, p1);
 
-                        Assert.IsTrue(edge.Length == edgeLine.Length);
-					}
-				}
-			}
+                        edgeLines.Add(edgeLine);
+                        edges.Add(edge);
+                    }
+                }
+            }
+            return edgeLines;
         }
     }
 }
