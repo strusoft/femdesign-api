0ff26825f1421dd0cdbcfbeb045278c1556dc55d
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-12-29 12:07:17 +0100

    Update GlobalAssemblyInfo.cs

--- FemDesign.Core/Properties/GlobalAssemblyInfo.cs
+++ FemDesign.Core/Properties/GlobalAssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("21.7.0.0")]
-[assembly: AssemblyFileVersion("21.7.0.0")]
+[assembly: AssemblyVersion("22.0.0.0")]
+[assembly: AssemblyFileVersion("22.0.0.0")]
