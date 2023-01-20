1754a2b37fb7fb37e83fbe5e1c4d1c0291d7caee
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2023-01-20 18:12:32 +0100

    :construction_worker: Bare Holes

---
+++ FemDesign.Core/Foundations/BoreHole.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using FemDesign.GenericClasses;
+using StruSoft.Interop.StruXml.Data;
+
+namespace FemDesign.Foundations
+{
+    [System.Serializable]
+    [XmlType(TypeName = "borehole")]
+    public partial class BoreHole : NamedEntityBase
+    {
+        [XmlIgnore]
+        private static int _boreHoleInstances = 0;
+        public static void ResetInstanceCount() => _boreHoleInstances = 0;
+        protected override int GetUniqueInstanceCount() => ++_boreHoleInstances;
+
+        [XmlElement]
+        public List<double> StrataLevels { get; set; }
+
+        [XmlAttribute("x")]
+        public double X { get; set; }
+
+        [XmlAttribute("y")]
+        public double Y { get; set; }
+
+        [XmlAttribute("final_ground_level")]
+        public double FinalGroundLevel { get; set; }
+
+        [XmlElement("whole_level_data")]
+        public AllLevels WholeLevelData { get; set; }
+
+        private BoreHole() { }
+
+        public BoreHole(double x, double y, double finalGroundLevel = 0.00, AllLevels allLevels = null, string identifier = "BH")
+        {
+            X = x;
+            Y = y;
+            FinalGroundLevel = finalGroundLevel;
+            WholeLevelData = allLevels;
+            Identifier = identifier;
+            this.EntityCreated();
+        }
+    }
+
+    public partial class AllLevels
+    {
+        [XmlElement("strata_top_levels")]
+        public string _strataTopLevels;
+
+        [XmlIgnore]
+        public List<double> StrataTopLevels
+        {
+            get
+            {
+                if (_strataTopLevels != null)
+                {
+                    return _strataTopLevels.Split(' ').Select(x => double.Parse(x)).ToList();
+                }
+                else return null;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    _strataTopLevels = String.Join(" ", value);
+                }
+            }
+        }
+
+        [XmlElement("water_levels")]
+        public string _waterLevels;
+
+        [XmlIgnore]
+        public List<double> WaterLevels
+        {
+            get
+            {
+                if (_waterLevels != null)
+                {
+                    return _waterLevels.Split(' ').Select(x => double.Parse(x)).ToList();
+                }
+                else return null;
+            }
+            set
+            {
+                if(value != null)
+                {
+                    _waterLevels = String.Join(" ", value);
+                }
+            }
+        }
+
+        public AllLevels() { }
+
+        public AllLevels(List<double> strataTopLevels, List<double> waterLevels)
+        {
+            StrataTopLevels = strataTopLevels;
+            WaterLevels = waterLevels;
+        }
+    }
+}
---
+++ FemDesign.Tests/Foundations/Foundations.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FemDesign;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using System.IO;
+namespace FemDesign.Foundations
+{
+    [TestClass]
+    public class FoundationsTests
+    {
+        [TestMethod]
+        public void CreateBoreholes()
+        {
+            var boreHole = new FemDesign.Foundations.BoreHole(10, 10, 0);
+
+            var strata = new List<double> { 0.00, -10.00, -20.00 };
+            var water = new List<double> { 0.00, -10.00, -20.00 };
+            Console.WriteLine( SerializeToString(boreHole) );
+
+            var levels = new FemDesign.Foundations.AllLevels(strata, water);
+            boreHole = new FemDesign.Foundations.BoreHole(10, 10, 0, levels);
+            Console.WriteLine(SerializeToString(boreHole));
+
+
+            levels = new FemDesign.Foundations.AllLevels(strata, null);
+            boreHole = new FemDesign.Foundations.BoreHole(10, 10, 0, levels);
+            Console.WriteLine(SerializeToString(boreHole));
+
+            boreHole = new FemDesign.Foundations.BoreHole(10, 10, 0);
+            Console.WriteLine(SerializeToString(boreHole));
+        }
+
+
+        public static string SerializeToString(BoreHole obj)
+        {
+            // serialize
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+            using (TextWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+    }
+}
