efdb687792051fcea5bc8ca8b8b20c6349c28287
lorinczandrea <lorincz.andrea@strusoft.hu>
2023-01-31 20:12:45 +0100

    #667 Component created

--- FemDesign.Grasshopper/Loads/LineTemperatureLoadDefineOBSOLETE.cs
+++ FemDesign.Grasshopper/Loads/LineTemperatureLoadDefineOBSOLETE.cs
@@ -6,19 +6,18 @@ using Rhino.Geometry;
 
 namespace FemDesign.Grasshopper
 {
-    public class LineTemperatureLoadDefine: GH_Component
+    public class LineTemperatureLoadDefineOBSOLETE: GH_Component
     {
-        public LineTemperatureLoadDefine(): base("LineTemperatureLoad.Define", "Define", "Define a surface temperature load", CategoryName.Name(), SubCategoryName.Cat3())
+        public LineTemperatureLoadDefineOBSOLETE(): base("LineTemperatureLoad.Define", "Define", "Define a surface temperature load", CategoryName.Name(), SubCategoryName.Cat3())
         {
 
         }
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddCurveParameter("Curve", "Curve", "Curve. Line or Arc.", GH_ParamAccess.item);
-            pManager.AddVectorParameter("Direction", "Dir", "Direction of load. If undefined global Y-axis will be used. For more information about direction - see FEM-Design GUI.", GH_ParamAccess.item);
+            pManager.AddVectorParameter("Direction", "Dir", "Direction of load. If undefined global Z-axis will be used. For more information about direction - see FEM-Design GUI.", GH_ParamAccess.item);
             pManager[pManager.ParamCount - 1].Optional = true;
-            pManager.AddNumberParameter("TopVal", "TopVal", "Top value. [\x00B0C]", GH_ParamAccess.item);
-            pManager.AddNumberParameter("BottomVal", "BottomVal", "Bottom value.  [\x00B0C]", GH_ParamAccess.item);
+            pManager.AddGenericParameter("TopBotLocationValue", "TopBotLocVal", "Temperature at top and bottom of surface. List should contain 2 values - start and end value. [\x00B0C]", GH_ParamAccess.list);
             pManager.AddGenericParameter("LoadCase", "LoadCase", "LoadCase.", GH_ParamAccess.item);
             pManager.AddTextParameter("Comment", "Comment", "Comment.", GH_ParamAccess.item);
             pManager[pManager.ParamCount - 1].Optional = true;
@@ -36,36 +35,26 @@ namespace FemDesign.Grasshopper
                 return;
             }
 
-            Vector3d dir = Vector3d.YAxis;
+            Vector3d dir = Vector3d.ZAxis;
             if (!DA.GetData(1, ref dir))
             {
                 // pass
             }
 
-            double topVal = 0;
-            if (!DA.GetData(2, ref topVal))
+            List<Loads.TopBotLocationValue> vals = new List<Loads.TopBotLocationValue>();
+            if (!DA.GetDataList(2, vals))
             {
                 return;
             }
 
-            double bottomVal = 0;
-            if (!DA.GetData(3, ref bottomVal))
-            {
-                return;
-            }
-
-            var val1 = new Loads.TopBotLocationValue(crv.PointAtStart.FromRhino(), topVal, bottomVal);
-            var val2 = new Loads.TopBotLocationValue(crv.PointAtEnd.FromRhino(), topVal, bottomVal);
-            var vals = new List<Loads.TopBotLocationValue>() { val1, val2 };
-            
             Loads.LoadCase lc = null;
-            if (!DA.GetData(4, ref lc))
+            if (!DA.GetData(3, ref lc))
             {
                 return;
             }
 
             string comment = null;
-            if (!DA.GetData(5, ref comment))
+            if (!DA.GetData(4, ref comment))
             {
                 // pass;
             }
@@ -95,10 +84,10 @@ namespace FemDesign.Grasshopper
         }
         public override Guid ComponentGuid
         {
-            get { return new Guid("C5A47F2F-6949-409A-A337-833D99A55234"); }
+            get { return new Guid("ac8a25ca-1b97-4d32-94f2-20a79575e298"); }
         }
 
-        public override GH_Exposure Exposure => GH_Exposure.secondary;
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
 
     }
 }
