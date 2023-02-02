6efb68887bfe861e2742ee4d6baa38047cc8cbf2
lorinczandrea <lorincz.andrea@strusoft.hu>
2023-02-02 14:37:50 +0100

    #677 LineTemperatureLoad is done

--- FemDesign.Grasshopper/Loads/LineTemperatureLoadDefine.cs
+++ FemDesign.Grasshopper/Loads/LineTemperatureLoadDefine.cs
@@ -15,10 +15,10 @@ namespace FemDesign.Grasshopper
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddCurveParameter("Curve", "Curve", "Curve. Line or Arc.", GH_ParamAccess.item);
-            pManager.AddVectorParameter("Direction", "Dir", "Direction of load. If undefined global Y-axis will be used. For more information about direction - see FEM-Design GUI.", GH_ParamAccess.item);
+            pManager.AddVectorParameter("Direction", "Dir", "Direction of load. If undefined local Y-axis will be used. For more information about direction - see FEM-Design GUI.", GH_ParamAccess.item);
             pManager[pManager.ParamCount - 1].Optional = true;
-            pManager.AddNumberParameter("TopVal", "TopVal", "Top value. [\x00B0C]", GH_ParamAccess.item);
-            pManager.AddNumberParameter("BottomVal", "BottomVal", "Bottom value.  [\x00B0C]", GH_ParamAccess.item);
+            pManager.AddNumberParameter("TopVal", "TopVal", "Top value. If 1 value this value defines both start and end. [\x00B0C]", GH_ParamAccess.list);
+            pManager.AddNumberParameter("BottomVal", "BottomVal", "Bottom value. If 1 value this value defines both start and end. [\x00B0C]", GH_ParamAccess.list);
             pManager.AddGenericParameter("LoadCase", "LoadCase", "LoadCase.", GH_ParamAccess.item);
             pManager.AddTextParameter("Comment", "Comment", "Comment.", GH_ParamAccess.item);
             pManager[pManager.ParamCount - 1].Optional = true;
@@ -37,34 +37,67 @@ namespace FemDesign.Grasshopper
             }
 
             Vector3d tan = new Vector3d(crv.PointAtEnd - crv.PointAtStart);
-            Vector3d dir = Vector3d.CrossProduct(tan, Vector3d.ZAxis);
+            Vector3d dir = new Vector3d();
+
+            //We have to use different default direction vector for a column (its tangent vector is parallel to the Z-axis) otherwise we will get an error message in default mode.
+            if (tan.IsParallelTo(Vector3d.ZAxis) == 1)
+            {
+                dir = Vector3d.CrossProduct(tan, -Vector3d.XAxis);
+            }
+            else
+            {
+                dir = Vector3d.CrossProduct(tan, Vector3d.ZAxis);
+            }
+
 
             if (!DA.GetData(1, ref dir))
             {
                 // pass
             }
 
-            if (DA.GetData(1, ref dir) & Vector3d.CrossProduct(dir, tan).IsZero)
+            if (DA.GetData(1, ref dir) & tan.IsParallelTo(dir) == 1)
             {
                 this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "The direction cannot be parallel to the curve.");
             }
 
-            double topVal = 0;
-            if (!DA.GetData(2, ref topVal))
+
+            List<double> topVal = new List<double>();
+            if (!DA.GetDataList(2, topVal))
             {
                 return;
             }
+            if ( topVal.Count == 1)
+            {
+                topVal.Add(topVal[0]);
+            }
+            if (topVal.Count > 2)
+            {
+                var msg = "Top value must contain 1 or 2 elements!";
+                this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, msg);
+                throw new System.ArgumentException(msg);
+            }
 
-            double bottomVal = 0;
-            if (!DA.GetData(3, ref bottomVal))
+            List<double> bottomVal = new List<double>();
+            if (!DA.GetDataList(3, bottomVal))
             {
                 return;
             }
+            if (bottomVal.Count == 1)
+            {
+                bottomVal.Add(bottomVal[0]);
+            }
+            if (bottomVal.Count > 2)
+            {
+                var msg = "Bottom value must contain 1 or 2 elements!";
+                this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, msg);
+                throw new System.ArgumentException(msg);
+            }
 
-            var firstValue = new Loads.TopBotLocationValue(crv.PointAtStart.FromRhino(), topVal, bottomVal);
-            var secondValue = new Loads.TopBotLocationValue(crv.PointAtEnd.FromRhino(), topVal, bottomVal);
-            var vals = new List<Loads.TopBotLocationValue>() { firstValue, secondValue };
+            var bottom = new Loads.TopBotLocationValue(crv.PointAtStart.FromRhino(), topVal[0], bottomVal[0]);
+            var top = new Loads.TopBotLocationValue(crv.PointAtEnd.FromRhino(), topVal[1], bottomVal[1]);
+            var vals = new List<Loads.TopBotLocationValue>() { top, bottom };
             
+
             Loads.LoadCase lc = null;
             if (!DA.GetData(4, ref lc))
             {
