1d5ceebf5f1ccfeda099a3c592139f5853870b15
andosca <andreas.osc@gmail.com>
2023-02-07 11:25:43 +0100

    first draft

---
+++ FemDesign.Core/Geometry/TextAnnotation.cs
@@ -0,0 +1,95 @@
+﻿
+
+using System;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+using StruSoft.Interop.StruXml.Data;
+namespace FemDesign.Geometry
+{
+    [Serializable]
+    public class TextAnnotation
+    {
+        [XmlElement("position")]
+        public Point3d Position { get; set; }
+        [XmlElement("local_x")]
+        public Vector3d LocalX { get; set; }
+        [XmlElement("local_y")]
+        public Vector3d LocalY { get; set; }
+        [XmlElement("style")]
+        public StruSoft.Interop.StruXml.Data.Style_type StyleType { get; set; }
+        [XmlAttribute("guid")]
+        public System.Guid Guid { get; set; }
+        [XmlAttribute("text")]
+        public string _text;
+        [XmlIgnore]
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                var regex = "[&#x0009;&#x000A;&#x000d; -&#xfffd;]{0,1023}";
+                var match = Regex.Match(value, regex);
+                if (match.Success)
+                {
+                    _text = value;
+                }
+                else
+                {
+                    throw new System.ArgumentException($"text value: {value} has bad formatting");
+                }
+            }
+        }
+        public TextAnnotation()
+        {
+
+        }
+        public void Initialize()
+        {
+            Position = Point3d.Origin;
+            LocalX = Vector3d.UnitX;
+            LocalY = Vector3d.UnitY;
+            StyleType = new Style_type
+            {
+                Layer = "0",
+                Font = new Font_type
+                {
+                    Script = Script_type.Default
+                }
+            };
+            Guid = System.Guid.NewGuid();
+        }
+
+        public TextAnnotation(Point3d position, Vector3d localX, Vector3d localY, string text)
+        {
+            this.Initialize();
+            Position = position;
+            LocalX = localX;
+            LocalY = localY;
+            Text = text;
+        }
+        public TextAnnotation(Point3d position, Vector3d localX, Vector3d localY, Style_type styleType, string text)
+        {
+            this.Initialize();
+            Position = position;
+            LocalX = localX;
+            LocalY = localY;
+            StyleType = styleType;
+            Text = text;
+        }
+    }
+}
+
+ //< xs:complexType name = "text_type" >
+ //  < xs:sequence >
+ //    < xs:element name = "position" type="point_type_3d" /> 
+ //    <xs:element name = "local_x"  type="point_type_3d" /> 
+ //    <xs:element name = "local_y"  type="point_type_3d" /> 
+ //    <xs:element name = "style"    type="style_type" /> 
+ //  </xs:sequence >
+ //  < xs:attribute name = "guid"      type="guidtype"          use="required" /> 
+ //  <xs:attribute name = "text"      type="geometry_text1023" use="required" /> 
+ //  <xs:anyAttribute processContents = "skip" />
+ //</ xs:complexType >
---
+++ FemDesign.Grasshopper/Geometry/TextAnnotation.cs
@@ -0,0 +1,80 @@
+﻿// https://strusoft.com/
+using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace FemDesign.Grasshopper
+{
+    public class TextAnnotation : GH_Component
+    {
+        public TextAnnotation() : base("TextAnnotation", "TextAnnotation", "Create a text annotation.", CategoryName.Name(), SubCategoryName.CatLast())
+        {
+
+        }
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("TextAnnotation", "TextAnnotation", "TextAnnotation", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddPlaneParameter("Point|Plane", "Point|Plane", "Position and orientation of text. [m]", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddTextParameter("Text", "Text", "Text.", GH_ParamAccess.item);
+        }
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("TextAnnotation", "TextAnnotation", "TextAnnotation.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddPlaneParameter("Plane", "Plane", "Position and orientation of text. [m]", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddTextParameter("Text", "Text", "Text.", GH_ParamAccess.item);
+        }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var textAnnot = new FemDesign.Geometry.TextAnnotation();
+            if (DA.GetData(0, ref textAnnot))
+            {
+                // pass
+            }
+            else
+            {
+                textAnnot.Initialize();
+            }
+
+            var plane = Plane.WorldXY;
+            if (!DA.GetData(1, ref plane)) 
+            { 
+                // pass
+            }
+
+            string text = null;
+            if (!DA.GetData(2, ref text))
+            {
+                return;
+            }
+
+            textAnnot.Position = plane.Origin.FromRhino();
+            textAnnot.LocalX = plane.XAxis.FromRhino();
+            textAnnot.LocalY = plane.YAxis.FromRhino();
+            textAnnot.Text = text;
+
+            DA.SetData(0, textAnnot);
+            DA.SetData(1, new Plane(textAnnot.Position.ToRhino(), textAnnot.LocalX.ToRhino(), textAnnot.LocalY.ToRhino()));
+            DA.SetData(2, textAnnot.Text);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("24767AB6-9D7E-4DE3-BB1A-5F0AF6470987"); }
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.septenary;
+
+    }
+}
