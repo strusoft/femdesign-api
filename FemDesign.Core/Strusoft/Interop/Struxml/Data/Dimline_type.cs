5dda6c6b574a2da6e1ee2732c60ebe5253c4620a
andosca <andreas.osc@gmail.com>
2023-02-20 11:24:02 +0100

    #661 add missing attributes to partial def

---
+++ FemDesign.Core/Strusoft/Interop/Struxml/Data/Dimline_type.cs
@@ -0,0 +1,38 @@
+﻿namespace StruSoft.Interop.StruXml.Data
+{
+    public partial class Dimline_type
+    {  
+        private System.DateTime last_changeField;
+
+        private Modification_type actionField;
+        
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute(AttributeName = "last_change")]
+        public System.DateTime Last_change
+        {
+            get
+            {
+                return this.last_changeField;
+            }
+            set
+            {
+                this.last_changeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute(AttributeName = "action")]
+        public Modification_type Action
+        {
+            get
+            {
+                return this.actionField;
+            }
+            set
+            {
+                this.actionField = value;
+            }
+        }
+
+    }
+}
