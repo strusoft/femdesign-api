6252f98f5892abcea26f89dc6ed69abd7ba82914
andosca <andreas.osc@gmail.com>
2023-02-22 09:44:46 +0100

    #661 add arrow type

--- FemDesign.Grasshopper/Drawing/DimensionLinear.cs
+++ FemDesign.Grasshopper/Drawing/DimensionLinear.cs
@@ -1,16 +1,29 @@
-﻿// https://strusoft.com/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Eto.Forms;
-using FemDesign.Grasshopper.Extension.ComponentExtension;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
+
+using Struxml = StruSoft.Interop.StruXml.Data;
+using FemDesign.Grasshopper.Extension.ComponentExtension;
 
 namespace FemDesign.Grasshopper
 {
     public class DimensionLinear : GH_Component
     {
+        public static readonly List<string> ArrowTypeValueList = Enum.GetNames(typeof(Struxml.Arrowtype_type)).ToList();
+        public static string ArrowTypeValueListDescription
+        {
+            get
+            {
+                var str = "";
+                foreach (var a in ArrowTypeValueList)
+                {
+                    str += "\n" + a;
+                }
+                return str;
+            }
+        }
         public DimensionLinear() : base("LinearDimension", "LnDim", "Create a linear dimension.", CategoryName.Name(), "ModellingTools")
         {
 
@@ -19,10 +32,19 @@ namespace FemDesign.Grasshopper
         {
             pManager.AddPlaneParameter("Point|Plane", "Point|Plane", "Position of dimension line and orientation of dimension. Distances will be measured along the plane x-axis.", GH_ParamAccess.item);
             pManager.AddPointParameter("ReferencePoints", "RefPoints", "Points on dimension line to measure between along plane X-axis.", GH_ParamAccess.list);
+            pManager.AddNumberParameter("FontSize", "FontSize", "Font size of text. [m]", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddNumberParameter("ArrowType", "ArrowType", $"Dimension line arrow type. Connect 'ValueList' to get the options: {ArrowTypeValueListDescription}", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
         }
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
             pManager.AddGenericParameter("LinearDimension", "LnDim", "Linear dimension.", GH_ParamAccess.item);
+        }
+
+        protected override void BeforeSolveInstance()
+        {
+            ValueListUtils.updateValueLists(this, 3, ArrowTypeValueList, null, 0);
         }
         protected override void SolveInstance(IGH_DataAccess DA)
         {
@@ -39,6 +61,25 @@ namespace FemDesign.Grasshopper
             }
 
             var dim = new Drawing.DimensionLinear(refPoints.Select(x => x.FromRhino()).ToList(), plane.ToPlane());
+
+            double size = 0;
+            if (DA.GetData(2, ref size))
+            {
+                dim.Font.Size = size;    
+            }
+
+            string arrowType = null;
+            if (DA.GetData(3, ref arrowType))
+            {
+                if (Enum.TryParse(arrowType, out Struxml.Arrowtype_type arrowTypeEnum))
+                {
+                    dim.Arrow.Type = arrowTypeEnum;
+                }
+                else
+                {
+                    throw new System.ArgumentException($"Invalid horisontal alignment value: {arrowType}, must be one of the following values: {ArrowTypeValueListDescription}");
+                }
+            }
 
             DA.SetData(0, dim);
         }
@@ -70,14 +111,23 @@ namespace FemDesign.Grasshopper
             pManager[pManager.ParamCount - 1].Optional = true;
             pManager.AddPointParameter("ReferencePoints", "RefPoints", "Points on dimension line to measure between.", GH_ParamAccess.list);
             pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddNumberParameter("FontSize", "FontSize", "Font size of text. [m]", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+            pManager.AddNumberParameter("ArrowType", "ArrowType", $"Dimension line arrow type. Connect 'ValueList' to get the options: {DimensionLinear.ArrowTypeValueListDescription}", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
         }
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
             pManager.AddGenericParameter("LinearDimension", "LnDim", "Linear dimension.", GH_ParamAccess.item);
             pManager.AddPlaneParameter("Plane", "Plane", "Position and orientation of text. [m]", GH_ParamAccess.item);
             pManager.AddPointParameter("ReferencePoints", "RefPoints", "Points on dimension line to measure between.", GH_ParamAccess.list);
-            pManager.AddNumberParameter("Distances", "Dists", "Distances between reference point along plane X-axis.", GH_ParamAccess.list);
+            pManager.AddNumberParameter("FontSize", "FontSize", "Font size of text. [m]", GH_ParamAccess.item);
+            pManager.AddTextParameter("ArrowType", "ArrowType", $"Dimension line arrow type.", GH_ParamAccess.item);
+        }
 
+        protected override void BeforeSolveInstance()
+        {
+            ValueListUtils.updateValueLists(this, 4, DimensionLinear.ArrowTypeValueList, null, 0);
         }
         protected override void SolveInstance(IGH_DataAccess DA)
         {
@@ -99,10 +149,30 @@ namespace FemDesign.Grasshopper
                 dim.ReferencePoints = refPoints.Select(x => x.FromRhino()).ToList();
             }
 
+            double size = dim.Font.Size;
+            if (DA.GetData(3, ref size))
+            {
+                dim.Font.Size = size;    
+            }
+
+            string arrowType = dim.Arrow.Type.ToString();
+            if (DA.GetData(4, ref arrowType))
+            {
+                if (Enum.TryParse(arrowType, out Struxml.Arrowtype_type arrowTypeEnum))
+                {
+                    dim.Arrow.Type = arrowTypeEnum;
+                }
+                else
+                {
+                    throw new System.ArgumentException($"Invalid horisontal alignment value: {arrowType}, must be one of the following values: {DimensionLinear.ArrowTypeValueListDescription}");
+                }
+            }
+
             DA.SetData(0, dim);
             DA.SetData(1, plane);
             DA.SetDataList(2, refPoints);
-            DA.SetDataList(3, dim.Distances);
+            DA.SetData(3, size);
+            DA.SetData(4, arrowType);
         }
 
         protected override System.Drawing.Bitmap Icon
