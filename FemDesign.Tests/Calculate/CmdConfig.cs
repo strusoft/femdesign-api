7682461e089e6f0965bcfde41e7c1de521d30c0f
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2023-05-12 11:06:48 +0200

    :construction_worker: cmddesignparametersteel

--- FemDesign.Tests/Calculate/CmdConfig.cs
+++ FemDesign.Tests/Calculate/CmdConfig.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using FemDesign;
 using FemDesign.Calculate;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace FemDesign.Calculate
 {
@@ -23,5 +25,27 @@ namespace FemDesign.Calculate
             var config = new FemDesign.Calculate.CmdConfig(cmsConfig, coCoConfig, ecst);
         }
 
+        [TestMethod("DesignParameter")]
+        public void DesignParameter()
+        {
+
+            var databse = Sections.SectionDatabase.GetDefault();
+
+            var mySection = databse.Sections.Section[0];
+            var mySection1 = databse.Sections.Section[1];
+
+            var sections = new List<Sections.Section> { mySection, mySection1};
+
+            var cmsConfig = new Calculate.DesParamBarSteel(0.90, sections);
+
+            var config = new FemDesign.Calculate.CmdConfig(cmsConfig);
+
+            var serializer = new XmlSerializer(typeof(CmdConfig));
+            var stringWriter = new StringWriter();
+            serializer.Serialize(stringWriter, config);
+
+            string serializedPerson = stringWriter.ToString();
+            Console.WriteLine(serializedPerson);
+        }
     }
 }
