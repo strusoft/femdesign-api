6cd0a40e6e7ea9ca9dc7a7c017c3bab8026cbd3e
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2023-05-22 16:23:51 +0200

    added test for interaction surface

---
+++ FemDesign.Tests/Reinforcement/InteractionSurfceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using FemDesign;
+
+namespace FemDesign.Reinforcement
+{
+    [TestClass]
+    public class InteractionSurface
+    {
+        [TestCategory("FEM-Design required")]
+        [TestMethod]
+        public void Deserialise()
+        {
+            var filePath = "Reinforcement\\intSrf-1.txt";
+            var results = FemDesign.Results.InteractionSurface.ReadFromFile(filePath);
+
+
+            var a = 2;
+
+            filePath = "Reinforcement\\intSrf-2.txt";
+            results = FemDesign.Results.InteractionSurface.ReadFromFile(filePath);
+
+            a = 3;
+
+            filePath = "Reinforcement\\intSrf.txt7cb0b0b6-96e9-4097-a8b6-bf5b8ae1b15c";
+            results = FemDesign.Results.InteractionSurface.ReadFromFile(filePath);
+
+            a = 3;
+        }
+    }
+}
