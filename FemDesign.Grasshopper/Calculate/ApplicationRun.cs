2fe40416de61a0b4e6336bc8de016bc37bfdf74b
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2023-06-06 16:39:29 +0200

    :construction_worker: RunAnalysis/Design for optimisation
    #787

---
+++ FemDesign.Grasshopper/Calculate/ApplicationRun.cs
@@ -0,0 +1,187 @@
+﻿// https://strusoft.com/
+using System;
+using System.Collections.Generic;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+using FemDesign.Loads;
+using FemDesign.Calculate;
+using FemDesign.Results;
+using System.Data.Common;
+using System.Reflection;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+
+namespace FemDesign.Grasshopper
+{
+    public class ApplicationRun : GH_Component
+    {
+        public ApplicationRun() : base("Application.Run", "Run", "Run a model. .csv list files and .docx documentation files are saved in the same work directory as StruxmlPath.", CategoryName.Name(), SubCategoryName.Cat7a())
+        {
+
+        }
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Model", "Model", "Model to open.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddGenericParameter("Analysis", "Analysis", "Analysis.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddGenericParameter("Design", "Design", "Design.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddGenericParameter("Mode", "Mode", "Design mode: rc, steel or timber.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddGenericParameter("DesignGroups", "DesignGroups", "DesignGroups.", GH_ParamAccess.list);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddTextParameter("ResultTypes", "ResultTypes", "Results to be extracted from model. This might require the model to have been analysed. Item or list.", GH_ParamAccess.list);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddGenericParameter("Units", "Units", "Specify the Result Units for some specific type. \n" +
+                "Default Units are: Length.m, Angle.deg, SectionalData.m, Force.kN, Mass.kg, Displacement.m, Stress.Pa", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddTextParameter("DocxTemplatePath", "DocxTemplatePath", "File path to documenation template file (.dsc) to run. Optional parameter.", GH_ParamAccess.item);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+            pManager.AddBooleanParameter("RunNode", "RunNode", "If true node will execute. If false node will not execute.", GH_ParamAccess.item, false);
+            pManager[pManager.ParamCount - 1].Optional = true;
+
+        }
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Model", "Model", "Model.", GH_ParamAccess.item);
+            pManager.Register_GenericParam("FiniteElement", "FiniteElement", "FemDesign Finite Element Geometries(nodes, bars, shells).");
+            pManager.AddGenericParameter("Results", "Results", "Results.", GH_ParamAccess.tree);
+        }
+
+
+        public dynamic _getResults(FemDesignConnection connection, Type resultType, Results.UnitResults units = null, Options options = null, List<FemDesign.GenericClasses.IStructureElement> elements = null)
+        {
+            var method = nameof(FemDesign.FemDesignConnection.GetResults);
+            List<Results.IResult> mixedResults = new List<Results.IResult>();
+            MethodInfo genericMethod = typeof(FemDesign.FemDesignConnection).GetMethod(method).MakeGenericMethod(resultType);
+            dynamic result = genericMethod.Invoke(connection, new object[] { units, options, elements });
+            mixedResults.AddRange(result);
+            return mixedResults;
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            dynamic _model = null;
+            DA.GetData("Model", ref _model);
+
+            FemDesign.Calculate.Analysis analysis = null;
+            DA.GetData("Analysis", ref analysis);
+
+            FemDesign.Calculate.Design design = null;
+            DA.GetData("Design", ref design);
+
+            string _userModule = null;
+            DA.GetData("Mode", ref _userModule);
+
+            CmdUserModule userModule = FemDesign.GenericClasses.EnumParser.Parse<CmdUserModule>(_userModule);
+
+            FemDesign.Results.UnitResults units = UnitResults.Default();
+            DA.GetData("Units", ref units);
+
+            List<FemDesign.Calculate.CmdDesignGroup> designGroups = new List<Calculate.CmdDesignGroup>();
+            DA.GetDataList("DesignGroups", designGroups);
+
+            // to make it as list
+            List<string> _resultType = new List<string>();
+            DA.GetDataList("ResultTypes", _resultType);
+
+            // Collect Outputs
+            Model model = null;
+            List<Results.IResult> results = new List<Results.IResult>();
+            FemDesign.Results.FiniteElement finiteElement = null;
+            var resultsTree = new DataTree<object>();
+
+
+            var types = new List<Type>();
+            foreach (var resType in _resultType)
+            {
+                var _type = $"FemDesign.Results.{resType}, FemDesign.Core";
+                Type type = Type.GetType(_type);
+                types.Add(type);
+            }
+
+            // Create Task
+            var t = Task.Run(() =>
+            {
+                var connection = new FemDesign.FemDesignConnection(minimized: false);
+
+                connection.Open(_model.Value);
+
+                if(analysis != null)
+                    connection.RunAnalysis(analysis);
+
+                if(design != null)
+                {
+                    if(designGroups.Count() == 0)
+                        connection.RunDesign(userModule, design);
+                    else
+                        connection.RunDesign(userModule, design, designGroups);
+                }
+
+                finiteElement = connection.GetFeaModel(units.Length);
+
+                if(types.Count != 0)
+                {
+                    int i = 0;
+                    foreach (var type in types)
+                    {
+                        var res = _getResults(connection, type, units);
+                        resultsTree.AddRange(res, new GH_Path(i));
+                        i++;
+                    }
+                }
+
+                if (_model.Value is string)
+                {
+                    model = connection.GetModel();
+                }
+
+                connection.Dispose();
+            });
+
+            
+            t.ConfigureAwait(false);
+            try
+            {
+                t.Wait();
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+
+            // Set output
+            DA.SetData("Model", model);
+            DA.SetData("FiniteElement", finiteElement);
+            DA.SetDataTree(2, resultsTree);
+        }
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return FemDesign.Properties.Resources.ModelRunAnalysis;
+            }
+        }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{D671E128-439D-43A5-B19C-01AAC57851AF}"); }
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+    }
+}
