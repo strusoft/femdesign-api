a523a513e820ea0ed24102f4a59db3061d8e260e
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2023-11-01 16:49:08 +0100

    :sparkles: save as
    #883

---
+++ FemDesign.Grasshopper/Pipe/PipeSaveAs.cs
@@ -0,0 +1,104 @@
+﻿// https://strusoft.com/
+using System;
+using System.Collections.Generic;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using System.Linq;
+using System.Windows.Forms;
+using FemDesign.Grasshopper.Extension.ComponentExtension;
+using GrasshopperAsyncComponent;
+using System.Reflection;
+
+namespace FemDesign.Grasshopper
+{
+    public class PipSave : GH_AsyncComponent
+    {
+        public PipSave() : base("FEM-Design.Save", "Save", "Save a model.", CategoryName.Name(), SubCategoryName.Cat8())
+        {
+            BaseWorker = new ApplicationSaveAs(this);
+        }
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Connection", "Connection", "FEM-Design connection.", GH_ParamAccess.item);
+            pManager.AddTextParameter("FilePath", "FilePath", "Save the model to .struxml or .str file", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("RunNode", "RunNode", "If true node will execute. If false node will not execute.", GH_ParamAccess.item, true);
+            pManager[pManager.ParamCount - 1].Optional = true;
+        }
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Connection", "Connection", "FEM-Design connection.", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Success", "Success", "True if session has exited. False if session is open or was closed manually.", GH_ParamAccess.item);
+        }
+
+        protected override System.Drawing.Bitmap Icon => FemDesign.Properties.Resources.ModelSave;
+        public override Guid ComponentGuid => new Guid("{473C29D5-4021-4D26-8397-56035D3EBC95}");
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+    }
+
+    public class ApplicationSaveAs : WorkerInstance
+    {
+        /* INPUT/OUTPUT */
+        private FemDesignConnection _connection = null;
+        private bool _runNode = true;
+        private bool _success = false;
+        private string _filePath = null;
+
+
+        public ApplicationSaveAs(GH_Component component) : base(component) { }
+
+
+        public override void DoWork(Action<string, string> ReportProgress, Action Done)
+        {
+            if (_runNode == false)
+            {
+                _success = false;
+                Parent.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Run node set to false.");
+                ReportProgress(Id, 0.0.ToString());
+                return;
+            }
+
+            if (_connection == null)
+            {
+                _success = false;
+                Parent.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Connection is null.");
+                return;
+            }
+
+            if (_connection.IsDisconnected)
+            {
+                _success = false;
+                Parent.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Connection to FEM-Design have been lost.");
+                return;
+            }
+
+            if (_connection.HasExited)
+            {
+                _success = false;
+                Parent.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "FEM-Design have been closed.");
+                return;
+            }
+
+            // Save the model
+            _connection.Save(_filePath);
+            _success = true;
+
+            Done();
+        }
+
+        public override WorkerInstance Duplicate() => new ApplicationSaveAs(Parent);
+
+        public override void GetData(IGH_DataAccess DA, GH_ComponentParamServer Params)
+        {
+            if (!DA.GetData("Connection", ref _connection)) return;
+            DA.GetData("FilePath", ref _filePath);
+            DA.GetData("RunNode", ref _runNode);
+        }
+
+        public override void SetData(IGH_DataAccess DA)
+        {
+            DA.SetData("Connection", _connection);
+            DA.SetData("Success", _success);
+        }
+    }
+}
