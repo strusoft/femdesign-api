d32e0bcbc386b971ddc2a246fc37d00ce51006e2
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2024-03-27 12:05:52 +0100

    :lipstick: update comb for plasticity

--- FemDesign.Core/Calculate/Comb.cs
+++ FemDesign.Core/Calculate/Comb.cs
@@ -13,30 +13,95 @@ namespace FemDesign.Calculate
     /// </summary>
     public partial class Comb
     {
+        /// <summary>
+        /// Maximum iteration number
+        /// </summary>
         [XmlAttribute("NLEmaxiter")]
-        public int NLEmaxiter { get; set; } // int
+        public int NLEmaxiter { get; set; } = 30;
+        
+        /// <summary>
+        /// Deafult load step in % of the total load
+        /// </summary>
         [XmlAttribute("PLdefloadstep")]
-        public int PLdefloadstep { get; set; } // int
+        public int PLdefloadstep { get; set; } = 20;
+
+        /// <summary>
+        /// Minimum load step [%]
+        /// </summary>
         [XmlAttribute("PLminloadstep")]
-        public int PLminloadstep { get; set; } // int
+        public int PLminloadstep { get; set; } = 2;
+
+        /// <summary>
+        /// Keep reduced load step after it has been reduced by the solver
+        /// </summary>
+        [XmlAttribute("PlKeepLoadStep")]
+        public bool PlKeepLoadStep { get; set; } = true;
+
+        /// <summary>
+        /// Golbal tolerance value [‰]
+        /// </summary>
+        [XmlAttribute("PlTolerance")]
+        public int PlTolerance { get; set; } = 1;
+
+        /// <summary>
+        /// Maximum equilibrium iteration number
+        /// </summary>
         [XmlAttribute("PLmaxeqiter")]
-        public int PLmaxeqiter { get; set; } // int
-        [XmlAttribute("CRloadstep")]
-        public int CRloadstep { get; set; } // int
-        [XmlAttribute("CRmaxiter")]
-        public int CRmaxiter { get; set; } // int
-        [XmlAttribute("CRstifferror")]
-        public int CRstifferror { get; set; } // int
+        public int PLmaxeqiter { get; set; } = 50;
+
+        /// <summary>
+        /// Number of layers in the elasto-plastic shells
+        /// </summary>
+        [XmlAttribute("PlShellLayers")]
+        public int PlShellLayers { get; set; } = 10;
+        
+        /// <summary>
+        /// Consider Mohr-Coulomb criteria
+        /// </summary>
         [XmlAttribute("NLSMohr")]
-        public bool NLSMohr { get; set; } // bool
+        public bool NLSMohr { get; set; } = true;
+        
+        /// <summary>
+        /// Initial load step [%]
+        /// </summary>
         [XmlAttribute("NLSinitloadstep")]
-        public int NLSinitloadstep { get; set; } // int
+        public int NLSinitloadstep { get; set; } = 10;
+        
+        /// <summary>
+        /// Minimum load step [%]
+        /// </summary>
         [XmlAttribute("NLSminloadstep")]
-        public int NLSminloadstep { get; set; } // int
+        public int NLSminloadstep { get; set; } = 10;
+        
+        /// <summary>
+        /// Volume ratio of nonlinearly active elements in one step [%]
+        /// </summary>
         [XmlAttribute("NLSactiveelemratio")]
-        public int NLSactiveelemratio { get; set; } // int
+        public int NLSactiveelemratio { get; set; } = 5;
+        
+        /// <summary>
+        /// Volume ratio of plastic elements in one step [%]
+        /// </summary>
         [XmlAttribute("NLSplasticelemratio")]
-        public int NLSplasticelemratio { get; set; } // int
+        public int NLSplasticelemratio { get; set; } = 5;
+
+        /// <summary>
+        /// One load step in % of the total load
+        /// </summary>
+        [XmlAttribute("CRloadstep")]
+        public int CRloadstep { get; set; } = 20;
+
+        /// <summary>
+        /// Maximum iteration number
+        /// </summary>
+        [XmlAttribute("CRmaxiter")]
+        public int CRmaxiter { get; set; } = 30;
+        
+        /// <summary>
+        /// Allowed stiffness change error [%]
+        /// </summary>
+        [XmlAttribute("CRstifferror")]
+        public int CRstifferror { get; set; } = 2;
 
         [XmlElement("combitem")]
         public List<CombItem> CombItem { get; set; }
