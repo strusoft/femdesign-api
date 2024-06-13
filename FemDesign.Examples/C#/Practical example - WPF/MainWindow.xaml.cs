1631020376ba92a32b019374e8b59e0023ec4470
MP <pellegrino.marco@icloud.com>
2024-06-13 15:35:28 +0200

    :sparkles: WPF #977

---
+++ FemDesign.Examples/C#/Practical example - WPF/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FemDesign;
+using FemDesign.Calculate;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using FemDesign.Results;
+
+namespace Practical_example___WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FemDesign.FemDesignConnection connection;
+        string _filePath;
+        Comb Comb = null;
+        Model model = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.connection = new FemDesign.FemDesignConnection(minimized: false);
+        }
+
+        ~MainWindow()
+        {
+            this.connection.Dispose();
+        }
+
+
+        private async Task OpenFemDesignFile(string filePath)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                connection.Open(filePath);
+            });
+        }
+        private async Task GetModel()
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                model = connection.GetModel();
+            });
+        }
+        private async Task RunOptimisation(int iteration, double thicknessStep)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+
+                // create a while loop with a condition that will be true until the utilisation is less than 1
+                double utilisation = 2;
+                int i = 1;
+
+                
+
+                while(utilisation > 1 && i < iteration)
+                {
+                    var thickness = i * thicknessStep / 1000;
+                    // update the thickness of all the slabs in the model
+                    foreach (var slab in model.Entities.Slabs)
+                    {
+                        slab.UpdateThickness(i * thickness);
+                    }
+
+                    connection.Open(model);
+                    var analysis = new Analysis(comb: Comb, calcCase: true, calcComb: true);
+                    connection.RunAnalysis(analysis);
+
+                    var quantities = connection.GetQuantities<FemDesign.Results.QuantityEstimationSteel>();
+                    double total = quantities.Sum(r => r.TotalWeight);
+
+                    var units = UnitResults.Default();
+                    units.Stress = Stress.MPa;
+                    var maxStress = connection.GetResults<ShellStress>(units, options: new Options(BarResultPosition.ResultPoints, ShellResultPosition.ResultPoints, 0.5)).Select(x => Math.Abs(x.SigmaVM)).Max();
+                    utilisation = maxStress / 355;
+
+                    DisplayResults(i, thickness, total, utilisation);
+                    i++;
+                }
+
+
+
+
+
+                //for (int i = 1; i <= iteration; i++)
+                //{
+                //    // update the thickness of all the slabs in the model
+                //    foreach (var slab in model.Entities.Slabs)
+                //    {
+                //        slab.UpdateThickness(i * thickness / 1000);
+                //    }
+                //    connection.Open(model);
+                //    var analysis = new Analysis(comb: Comb, calcCase: true, calcComb: true);
+                //    connection.RunAnalysis(analysis);
+
+                //    var quantities = connection.GetQuantities<FemDesign.Results.QuantityEstimationSteel>();
+                //    double total = quantities.Sum(r => r.TotalWeight);
+
+                //    var units = UnitResults.Default();
+                //    units.Stress = Stress.MPa;
+                //    var maxStress = connection.GetResults<ShellStress>(options: new Options(BarResultPosition.ResultPoints, ShellResultPosition.ResultPoints, 0.5)).Select(x => Math.Abs(x.SigmaVM)).Max();
+                //    var utilisation = 355 / maxStress;
+
+                //    DisplayResults(i, thickness, total, utilisation);
+                //}
+            });
+        }
+        private void DisplayResults(int i, double thickness, double weight, double utilisation)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                TextBox.AppendText($"Iteration: {i}\r\n");
+                TextBox.AppendText($"thickness: {thickness}\r\n");
+                TextBox.AppendText($"Steel utilisation: {utilisation * 100:0.0} %.\r\n");
+                TextBox.AppendText($"Steel weight: {weight:0.0} ton.\r\n");
+                TextBox.AppendText($"\r\n");
+            });
+        }
+
+        #region Events
+        private async void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            // open a file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "FEM-Design files|*.str;*.struxml";
+            openFileDialog.InitialDirectory = "C:\\GitHub\\femdesign-api\\FemDesign.Examples\\C#\\Practical example - WPF";
+            openFileDialog.Title = "Select a FemDesign file";
+
+            // store the selected file path
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _filePath = openFileDialog.FileName;
+                this.label1.Content = System.IO.Path.GetFileName(_filePath);
+
+                await OpenFemDesignFile(_filePath);
+                await GetModel();
+                
+            }
+        }
+        private void Picture_MouseDown(object sender, MouseEventArgs e)
+        {
+            // open a browser to the FemDesign API documentation
+            System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("cmd", "/c start " + "https://femdesign-api-docs.onstrusoft.com/");
+            psi.CreateNoWindow = true;
+            System.Diagnostics.Process.Start(psi);
+        }
+        private async void RunOptimisation_Click(object sender, RoutedEventArgs e)
+        {
+            if(_filePath == null)
+            {
+                MessageBox.Show("Please open a FemDesign file first");
+                return;
+            }
+
+            Run_Optimisation.Content = "Processing...";
+            Run_Optimisation.IsEnabled = false;
+
+            TextBox.Clear();
+
+            int iteration = int.Parse(ComboBox.Text);
+            double thickness = double.Parse(Thk.Text);
+
+            // await makes sure that the task is completed before the next line is executed
+            await RunOptimisation(iteration, thickness);
+
+            // it will be called after the task is completed
+            TextBox.AppendText($"Optimisation completed");
+
+            Run_Optimisation.Content = "Run Optimisation";
+            Run_Optimisation.IsEnabled = true;
+        }
+        private void setComb()
+        {
+            if (Radio_button_NLE.IsChecked == true)
+            {
+                var comb = new Comb();
+                var NLE = new CombItem(NLE: true, PL: true);
+                comb.CombItem = new List<CombItem> { NLE };
+                Comb = comb;
+            }
+
+            if (Radio_button_LE.IsChecked == true)
+            {
+                var comb = new Comb();
+                var LE = new CombItem(NLE: false, PL: false);
+                comb.CombItem = new List<CombItem> { LE };
+                Comb = comb;
+            }
+        }
+        private void Radio_button_LE_Checked(object sender, RoutedEventArgs e)
+        {
+            setComb();
+        }
+        private void Radio_button_NLE_Checked(object sender, RoutedEventArgs e)
+        {
+            setComb();
+        }
+
+        #endregion
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FemDesign.Examples/C#/WinFormAdvance/Form1.cs
@@ -0,0 +1,211 @@
+﻿using FemDesign;
+using FemDesign.Calculate;
+using FemDesign.Results;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace WinFormAdvance
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        FemDesign.FemDesignConnection connection;
+        string _filePath;
+        Comb Comb;
+        Model model;
+
+        FemDesign.Calculate.Analysis analysisType = FemDesign.Calculate.Analysis.StaticAnalysis();
+
+        public Form()
+        {
+            InitializeComponent();
+            this.connection = new FemDesign.FemDesignConnection(minimized: true);
+            this.label1.Text = "No file selected";
+            this.comboBox1.SelectedIndex = 2;
+            this.radioButtonNLE.Checked = true;
+        }
+
+        ~Form()
+        {
+            this.connection.Dispose();
+            Dispose();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // open a browser to the FemDesign API documentation
+            System.Diagnostics.Process.Start("https://femdesign-api-docs.onstrusoft.com/");
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // open a file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "FEM-Design files|*.str;*.struxml";
+            openFileDialog.InitialDirectory = "C:\\Users\\Marco\\OneDrive - StruSoft AB\\FD API\\00_API_Development\\23\\23.3.0\\420-mass-point\\FEM-Design API";
+            openFileDialog.Title = "Select a FemDesign file";
+
+            // store the selected file path
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _filePath = openFileDialog.FileName;
+                this.label1.Text = Path.GetFileName(_filePath);
+
+                await OpenFemDesignFile(_filePath);
+                await GetModel();
+            }
+        }
+
+        // wrap all the femdesign calls in a task to avoid blocking the UI
+        private async Task OpenFemDesignFile(string filePath)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                connection.Open(filePath);
+            });
+        }
+
+        private async Task OpenFemDesignModel(Model model)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                connection.Open(model);
+            });
+        }
+
+        // write an async task to get the model
+        private async Task GetModel()
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                model = connection.GetModel();
+            });
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonLE_CheckedChanged(object sender, EventArgs e)
+        {
+            setComb();
+        }
+
+        private void radioButtonNLE_CheckedChanged(object sender, EventArgs e)
+        {
+            setComb();
+        }
+
+        private void setComb()
+        {
+            if (radioButtonNLE.Checked)
+            {
+                var comb = new Comb();
+                var NLE = new CombItem(NLE: true, PL: true);
+                comb.CombItem = new List<CombItem> { NLE };
+                Comb = comb;
+            }
+
+            if (radioButtonLE.Checked)
+            {
+                var comb = new Comb();
+                var LE = new CombItem(NLE: false, PL: false);
+                comb.CombItem = new List<CombItem> { LE };
+                Comb = comb;
+            }
+        }
+
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private async void buttonRunOptimisation_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+
+            var index = comboBox1.SelectedIndex;
+            var iteration = (int)comboBox1.Items[index];
+
+            // await makes sure that the task is completed before the next line is executed
+            await RunOptimisation(iteration);
+
+            // it will be called after the task is completed
+            textBox1.AppendText($"Optimisation completed");
+        }
+
+
+        private async Task RunOptimisation(int iteration)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                for (int i = 1; i <= iteration; i++)
+                {
+                    // update the thickness of all the slabs in the model
+                    foreach (var slab in model.Entities.Slabs)
+                    {
+                        slab.UpdateThickness(i * 0.01);
+                    }
+                    connection.Open(model);
+                    var analysis = new Analysis(comb: Comb, calcCase: true, calcComb: true);
+                    connection.RunAnalysis(analysis);
+
+                    var quantities = connection.GetQuantities<FemDesign.Results.QuantityEstimationSteel>();
+                    double total = quantities.Sum(r => r.TotalWeight);
+
+                    // display the results comes from a different thread so it is necessary to use Invoke
+                    DisplayResults(i, total);
+                }
+            });
+        }
+
+
+
+        private void DisplayResults(int i, double weight)
+        {
+            textBox1.Invoke((MethodInvoker)(() =>
+            {
+                textBox1.AppendText($"Iteration: {i}.\r\n");
+                textBox1.AppendText($"Steel weight: {weight:0.0} ton.\r\n");
+            }));
+        }
+
+
+        private void radioButton3_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
