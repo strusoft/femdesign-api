16f16dbff2f36cb3a806967ceba7df5f33db4734
lorinczandrea <lorincz.andrea@strusoft.hu>
2024-10-16 06:32:49 +0200

    :bug: remove deprecated composite test

--- FemDesign.Tests/Composites/CompositeBeamTest.cs
+++ FemDesign.Tests/Composites/CompositeBeamTest.cs
@@ -87,53 +87,6 @@ namespace FemDesign.Composites
             Assert.AreEqual(complexCompositesOut.Count, complexCompositesIn.Count);
         }
         
-        public void CompositeColumnA()
-        {
-            // Load material and sections from .struxml files
-            var materialsDB = Materials.MaterialDatabase.DeserializeStruxml(@"C:\Repos\femdesign-api\FemDesign.Tests\bin\Debug\Composites\materials.struxml");
-            var steel = materialsDB.MaterialByName("S 275");
-            var concrete = materialsDB.MaterialByName("C25/30");
-            var matList = new List<Materials.Material>() { steel, concrete };
-
-            var sectionsDB = Sections.SectionDatabase.DeserializeStruxml(@"C:\Repos\femdesign-api\FemDesign.Tests\bin\Debug\Composites\sections.struxml");
-            var steelSection = sectionsDB.SectionByName("Steel sections, HE-B, 300");
-            var concreteSection = sectionsDB.SectionByName("Unnamed, Concrete, 3");
-            var secList = new List<Sections.Section>() { steelSection, concreteSection };
-
-
-            // Create composite object
-            var composite = new Composites();
-
-            // Create composite section
-            string name = "TestColumnA1";
-            double cy = 80;     //mm
-            double cz = 80;     //mm
-            CompositeSection compositeSection = CompositeSection.FilledIProfile(steel, concrete, steelSection, name, cy, cz);
-
-            composite.CompositeSection = new List<CompositeSection>() { compositeSection };
-
-            //composite.CompositeSection[0].ParameterDictionary[CompositeParameterType.];
-            //var sth = GetCompositeSectionParameters(compositeSection);
-
-
-            // Create complex composite
-            ComplexComposite complexComposite = new ComplexComposite(compositeSection);
-            composite.ComplexComposite = new List<ComplexComposite>() { complexComposite };
-
-
-            // Serialize struxml
-            string fileName = "CompositeSerialization";
-            string filePath = @"D:\Andi\API_Work\0_Github\802_CompositeSections\tests\CSharp\" + fileName + ".struxml";
-            this.SerializeComposite(filePath, composite);
-
-            //Deserialize struxml
-            Composites inData = DeserializeComposite(filePath);
-
-            // Compare data
-            Assert.AreEqual(composite, inData);
-        }
-
-
 
         public void SerializeComposite(string filePath, Composites composite)
         {
